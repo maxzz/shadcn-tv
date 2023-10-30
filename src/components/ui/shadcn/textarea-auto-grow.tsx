0262dfb82a74fb132a2a9835ce7b60e6cffb2926
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-30 16:42:43 -0700

    WIP. update TextareaAutoGrow component

--- src/components/ui/shadcn/textarea-auto-grow.tsx
+++ src/components/ui/shadcn/textarea-auto-grow.tsx
@@ -19,8 +19,8 @@ export const containerPaddingFontClasses = 'after:px-3 after:py-2 after:text-sm 
 export const containerPaddingFontDebugClasses = "after:text-red-500 after:visible after:pointer-events-none";
 
 /**
- * Example:
  *  ```
+ *  // Example:
  *  const snap = useSnapshot(parseText, { sync: true });
  * 
  *  <TextareaAutoGrow
@@ -28,14 +28,13 @@ export const containerPaddingFontDebugClasses = "after:text-red-500 after:visibl
  *      onChange={(e) => parseText.text = e.target.value}
  *      rows={1}
  *      className="min-h-0"
- *      // For debugging:
  *      // textareaPaddingFont={cn(textareaPaddingFontDebugClasses, textareaPaddingFontClasses)}
  *      placeholder="Paste theme vars here"
  *      spellCheck={false}
  *  />
  *  ```
- * @param param0 
- * @returns 
+ * @param {string} containerPaddingFont "textareaPaddingFontDebugClasses" can be used for debugging. \
+ * Also additional styles can be provided to adjust container padding, font, or border.
  */
 export const TextareaAutoGrow = forwardRef<HTMLTextAreaElement, { containerPaddingFont?: string; } & TextareaProps>(
     ({ containerPaddingFont = containerPaddingFontClasses, className, value, ...rest }, ref) => {

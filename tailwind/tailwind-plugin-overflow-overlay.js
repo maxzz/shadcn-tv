61bdaabc037af22cee79bffa0977719b35e00a5f
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-30 16:22:51 -0700

    WIP. tailwind. tailwind-plugin-overflow-overlay new version

--- tailwind/tailwind-plugin-overflow-overlay.js
+++ tailwind/tailwind-plugin-overflow-overlay.js
@@ -1,8 +1,18 @@
 const plugin = require('tailwindcss/plugin');
 
-// Don't use plugin: tailwind cannot have two overlays: auto and overlay at the same time and will break for Firefox. Use index.css instead.
-
 module.exports = plugin(function ({ addUtilities }) {
+
+    /*
+    Don't use plugin: tailwind cannot have two overlays: auto and overlay at the same time and will break for Firefox. Use index.css instead: 
+        @layer utilities {
+            .overflow-overlay {
+                overflow: auto;
+                overflow: overlay;
+            }
+        }
+        //TODO: Is it still true? It is still true for tailwind 3.2.0.
+    */
+    
     const overlays = {
         '.overflow-overlay': {
             'overflow': 'auto',
@@ -15,6 +25,18 @@ module.exports = plugin(function ({ addUtilities }) {
         '.overflow-x-overlay': {
             'overflow-x': 'auto',
             'overflow-x': 'overlay',
+        },
+
+        "@supports (overflow: overlay)": {
+            ".overflow-overlay": {
+                'overflow': "overlay",
+            },
+            '.overflow-y-overlay': {
+                'overflow-y': 'overlay',
+            },
+            '.overflow-x-overlay': {
+                'overflow-x': 'overlay',
+            },
         }
     };
     addUtilities(overlays);
@@ -41,16 +63,24 @@ module.exports = plugin(function ({ addUtilities }) {
             borderRadius: "var(--sb-radius)"
         },
 
-        ".overflow-overlay": {
-            overflow: 'auto',
-            overflow: 'overlay',
+        ".smallscroll::-webkit-scrollbar-corner": {
+            backgroundColor: "transparent",
         },
-
-        "@supports (overflow: overlay)": {
-            ".overflow-overlay": {
-                overflow: "overlay",
-            }
-        }
     };
     addUtilities(smallscroll);
+
+    // Additional customizations can be done from the app like: [&::-webkit-resizer]:rounded
+    const resizer = {
+        ".resizer": {
+            "&::-webkit-resizer": {
+                backgroundColor: 'transparent',
+                backgroundImage: 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAAsSAAALEgHS3X78AAAAIGNIUk0AAG11AABzoAAA9q8AAIWZAABumgAA57sAADF9AAAXvQF2CngAAABESURBVHjajM67DQAgDENBw6x0WZDtHi3KR4klF5ausAAVvf8u0TGjgwFlMEUeepR+DCj72CJAC9AkW8OM4QMAAP//AwD5ltVB1vqf0gAAAABJRU5ErkJggg==")',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'bottom right',
+                backgroundSize: '95% 95%',
+            }
+        },
+    };
+    addUtilities(resizer);
+
 });

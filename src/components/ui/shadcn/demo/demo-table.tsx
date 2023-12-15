68211b0dbaa4cd708d91a18475093a8dff0d8ff5
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-14 18:38:53 -0800

    WIP. DifferentCases. update TableDemo

--- src/components/ui/shadcn/demo/demo-table.tsx
+++ src/components/ui/shadcn/demo/demo-table.tsx
@@ -8,6 +8,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/shadcn/table";
+import { HTMLAttributes } from "react";
 
 const invoices = [
     {
@@ -54,10 +55,13 @@ const invoices = [
     },
 ];
 
-export function TableDemo() {
+export function TableDemo(props: HTMLAttributes<HTMLTableElement>) {
     return (
-        <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
+        <Table {...props}>
+            <TableCaption>
+                A list of your recent invoices.
+            </TableCaption>
+
             <TableHeader>
                 <TableRow>
                     <TableHead className="w-[100px]">Invoice</TableHead>
@@ -66,6 +70,7 @@ export function TableDemo() {
                     <TableHead className="text-right">Amount</TableHead>
                 </TableRow>
             </TableHeader>
+
             <TableBody>
                 {invoices.map((invoice) => (
                     <TableRow key={invoice.invoice}>
@@ -76,7 +81,8 @@ export function TableDemo() {
                     </TableRow>
                 ))}
             </TableBody>
-            <TableFooter>
+
+            <TableFooter className="text-muted-foreground bg-muted">
                 <TableRow>
                     <TableCell colSpan={3}>Total</TableCell>
                     <TableCell className="text-right">$2,500.00</TableCell>

dd03f399ab08aa788276a0a5bc798298e6d14519
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-15 17:43:37 -0800

    WIP. shadcn. clean up DropdownMenu animation classes

--- src/components/ui/ui-dropdown-menu.tsx
+++ src/components/ui/ui-dropdown-menu.tsx
@@ -21,27 +21,7 @@ interface DropdownMenuProps {
 // radix-side-top:animate-slide-up \
 // radix-side-bottom:animate-slide-down \
 
-// export const popupAnimatonClasses = " \
-// data-[state=open]:animate-in \
-// data-[state=open]:fade-in-0 \
-// data-[state=open]:zoom-in-95 \
-// \
-// data-[state=closed]:animate-out \
-// data-[state=closed]:fade-out-0 \
-// data-[state=closed]:zoom-out-95 \
-// \
-// data-[side=bottom]:slide-in-from-top-2 \
-// data-[side=left]:slide-in-from-right-2 \
-// data-[side=right]:slide-in-from-left-2 \
-// data-[side=top]:slide-in-from-bottom-2 \
-// ";
-
-const contentClasses = `${" \
-p-1 \
-bg-background \
-\
-border rounded-md shadow-md \
-"} ${popupAnimatonClasses}`;
+const contentClasses = `${"p-1 bg-background border rounded-md shadow-md"} ${popupAnimatonClasses}`;
 
 const itemClasses = " \
 px-2 py-2 text-xs  \

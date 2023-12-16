bbb1c2ab6fdd5e35de018ce4a5f58ae2c282bce6
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-16 14:40:33 -0800

    WIP. shadcn. Tree component code review. 4

--- src/components/ui/shadcn/tree.tsx
+++ src/components/ui/shadcn/tree.tsx
@@ -5,7 +5,7 @@ import { ScrollArea } from "@/components/ui/shadcn/scroll-area";
 import { ChevronRight, type LucideIcon as LucideIconType } from "lucide-react";
 import { cn } from "@/utils";
 
-type TreeDataItem = {
+export type TreeDataItem = {
     id: string;
     name: string;
     icon?: LucideIconType,
@@ -21,38 +21,7 @@ type TreeProps = {
     iconItem?: LucideIconType;
 };
 
-function collectExpandedItemIds(data: TreeDataItem[] | TreeDataItem, initialSlelectedItemId: string | undefined, expandAll: boolean | undefined): string[] {
-    const rv: string[] = [];
-
-    if (initialSlelectedItemId) {
-        walkTreeItems(data, initialSlelectedItemId);
-    }
-
-    return rv;
-
-    function walkTreeItems(items: TreeDataItem[] | TreeDataItem, targetId: string) {
-        if (items instanceof Array) {
-            // eslint-disable-next-line @typescript-eslint/prefer-for-of
-            for (let i = 0; i < items.length; i++) {
-                rv.push(items[i]!.id);
-
-                if (walkTreeItems(items[i]!, targetId) && !expandAll) {
-                    return true;
-                }
-
-                if (!expandAll) {
-                    rv.pop();
-                }
-            }
-        } else if (!expandAll && items.id === targetId) {
-            return true;
-        } else if (items.children) {
-            return walkTreeItems(items.children, targetId);
-        }
-    }
-}
-
-const Tree = React.forwardRef<HTMLDivElement, TreeProps & React.HTMLAttributes<HTMLDivElement>>(
+export const Tree = React.forwardRef<HTMLDivElement, TreeProps & React.HTMLAttributes<HTMLDivElement>>(
     ({ data, initialSlelectedItemId, onSelectChange, expandAll, iconFolder: folderIcon, iconItem: itemIcon, className, ...rest }, ref) => {
         const [selectedItemId, setSelectedItemId] = React.useState(initialSlelectedItemId);
 
@@ -88,6 +57,37 @@ const Tree = React.forwardRef<HTMLDivElement, TreeProps & React.HTMLAttributes<H
     }
 );
 
+function collectExpandedItemIds(data: TreeDataItem[] | TreeDataItem, initialSlelectedItemId: string | undefined, expandAll: boolean | undefined): string[] {
+    const rv: string[] = [];
+
+    if (initialSlelectedItemId) {
+        walkTreeItems(data, initialSlelectedItemId);
+    }
+
+    return rv;
+
+    function walkTreeItems(items: TreeDataItem[] | TreeDataItem, targetId: string) {
+        if (items instanceof Array) {
+            // eslint-disable-next-line @typescript-eslint/prefer-for-of
+            for (let i = 0; i < items.length; i++) {
+                rv.push(items[i]!.id);
+
+                if (walkTreeItems(items[i]!, targetId) && !expandAll) {
+                    return true;
+                }
+
+                if (!expandAll) {
+                    rv.pop();
+                }
+            }
+        } else if (!expandAll && items.id === targetId) {
+            return true;
+        } else if (items.children) {
+            return walkTreeItems(items.children, targetId);
+        }
+    }
+}
+
 type TreeItemProps =
     & TreeProps
     & {
@@ -111,38 +111,39 @@ const TreeItem = React.forwardRef<HTMLDivElement, TreeItemProps & React.HTMLAttr
                         ? data.map(
                             (item) => (
                                 <li key={item.id}>
-                                    {item.children ? (
-                                        <A.Root type="multiple" defaultValue={expandedItemIds}>
-                                            <A.Item value={item.id}>
-                                                <AccordionTrigger
-                                                    className={cn(treeItemBaseClasses, selectedItemId === item.id && treeItemSelectedClasses)}
-                                                    onClick={() => handleSelectChange(item)}
-                                                >
-                                                    {item.icon && <item.icon className={treeItemIconClasses} aria-hidden="true" />}
-                                                    {!item.icon && FolderIcon && <FolderIcon className={treeItemIconClasses} aria-hidden="true" />}
-                                                    <span className="text-sm truncate">{item.name}</span>
-                                                </AccordionTrigger>
+                                    {item.children
+                                        ? (
+                                            <A.Root type="multiple" defaultValue={expandedItemIds}>
+                                                <A.Item value={item.id}>
+                                                    <TreeItemTrigger
+                                                        className={cn(treeItemBaseClasses, selectedItemId === item.id && treeItemSelectedClasses)}
+                                                        onClick={() => handleSelectChange(item)}
+                                                    >
+                                                        {item.icon && <item.icon className={treeItemIconClasses} aria-hidden="true" />}
+                                                        {!item.icon && FolderIcon && <FolderIcon className={treeItemIconClasses} aria-hidden="true" />}
+                                                        <span className="text-sm truncate">{item.name}</span>
+                                                    </TreeItemTrigger>
 
-                                                <AccordionContent className="pl-6">
-                                                    <TreeItem
-                                                        data={item.children ? item.children : item}
-                                                        selectedItemId={selectedItemId}
-                                                        handleSelectChange={handleSelectChange}
-                                                        expandedItemIds={expandedItemIds}
-                                                        FolderIcon={FolderIcon}
-                                                        ItemIcon={ItemIcon}
-                                                    />
-                                                </AccordionContent>
-                                            </A.Item>
-                                        </A.Root>
-                                    ) : (
-                                        <Leaf
-                                            item={item}
-                                            isSelected={selectedItemId === item.id}
-                                            onClick={() => handleSelectChange(item)}
-                                            Icon={ItemIcon}
-                                        />
-                                    )}
+                                                    <TreeItemContent className="pl-6">
+                                                        <TreeItem
+                                                            data={item.children ? item.children : item}
+                                                            selectedItemId={selectedItemId}
+                                                            handleSelectChange={handleSelectChange}
+                                                            expandedItemIds={expandedItemIds}
+                                                            FolderIcon={FolderIcon}
+                                                            ItemIcon={ItemIcon}
+                                                        />
+                                                    </TreeItemContent>
+                                                </A.Item>
+                                            </A.Root>
+                                        ) : (
+                                            <Leaf
+                                                item={item}
+                                                isSelected={selectedItemId === item.id}
+                                                onClick={() => handleSelectChange(item)}
+                                                Icon={ItemIcon}
+                                            />
+                                        )}
                                 </li>
                             )
                         )
@@ -163,37 +164,32 @@ const TreeItem = React.forwardRef<HTMLDivElement, TreeItemProps & React.HTMLAttr
     }
 );
 
+const leafBaseClasses = "flex items-center py-2 px-2 cursor-pointer hover:before:opacity-100 before:absolute before:left-0 before:right-1 before:w-full before:opacity-0 before:bg-muted/80 before:h-[1.75rem] before:-z-10";
+const leafSelectedClasses = "before:opacity-100 before:bg-accent text-accent-foreground before:border-l-2 before:border-l-accent-foreground/50 dark:before:border-0";
+
 const Leaf = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement> & { item: TreeDataItem, isSelected?: boolean, Icon?: LucideIconType; }>(
-    ({ className, item, isSelected, Icon, ...props }, ref) => {
+    ({ className, item, isSelected, Icon, ...rest }, ref) => {
         return (
-            <div
-                ref={ref}
-                className={cn(
-                    "flex items-center py-2 px-2 cursor-pointer \
-                hover:before:opacity-100 before:absolute before:left-0 before:right-1 before:w-full before:opacity-0 before:bg-muted/80 before:h-[1.75rem] before:-z-10",
-                    className,
-                    isSelected && "before:opacity-100 before:bg-accent text-accent-foreground before:border-l-2 before:border-l-accent-foreground/50 dark:before:border-0"
-                )}
-                {...props}
-            >
+            <div ref={ref} className={cn(leafBaseClasses, className, isSelected && leafSelectedClasses)} {...rest}>
+
                 {item.icon && <item.icon className="h-4 w-4 shrink-0 mr-2 text-accent-foreground/50" aria-hidden="true" />}
                 {!item.icon && Icon && <Icon className="h-4 w-4 shrink-0 mr-2 text-accent-foreground/50" aria-hidden="true" />}
-                <span className="flex-grow text-sm truncate">{item.name}</span>
+
+                <span className="flex-grow text-sm truncate">
+                    {item.name}
+                </span>
             </div>
         );
     }
 );
 
-const AccordionTrigger = React.forwardRef<React.ElementRef<typeof A.Trigger>, React.ComponentPropsWithoutRef<typeof A.Trigger>>(
-    ({ className, children, ...props }, ref) => (
+const TreeItemTrigger = React.forwardRef<React.ElementRef<typeof A.Trigger>, React.ComponentPropsWithoutRef<typeof A.Trigger>>(
+    ({ className, children, ...rest }, ref) => (
         <A.Header>
             <A.Trigger
                 ref={ref}
-                className={cn(
-                    "flex flex-1 w-full items-center py-2 transition-all last:[&[data-state=open]>svg]:rotate-90",
-                    className
-                )}
-                {...props}
+                className={cn("flex flex-1 w-full items-center py-2 transition-all last:[&[data-state=open]>svg]:rotate-90", className)}
+                {...rest}
             >
                 {children}
                 <ChevronRight className="h-4 w-4 shrink-0 transition-transform duration-200 text-accent-foreground/50 ml-auto" />
@@ -201,22 +197,17 @@ const AccordionTrigger = React.forwardRef<React.ElementRef<typeof A.Trigger>, Re
         </A.Header>
     )
 );
-AccordionTrigger.displayName = A.Trigger.displayName;
+TreeItemTrigger.displayName = A.Trigger.displayName;
 
-const AccordionContent = React.forwardRef<React.ElementRef<typeof A.Content>, React.ComponentPropsWithoutRef<typeof A.Content>>(
-    ({ className, children, ...props }, ref) => (
+const TreeItemContent = React.forwardRef<React.ElementRef<typeof A.Content>, React.ComponentPropsWithoutRef<typeof A.Content>>(
+    ({ className, children, ...rest }, ref) => (
         <A.Content
             ref={ref}
-            className={cn(
-                "overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down",
-                className
-            )}
-            {...props}
+            className={cn("overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down", className)}
+            {...rest}
         >
             <div className="pb-1 pt-0">{children}</div>
         </A.Content>
     )
 );
-AccordionContent.displayName = A.Content.displayName;
-
-export { Tree, type TreeDataItem };
+TreeItemContent.displayName = A.Content.displayName;

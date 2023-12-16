ce699f77538a2d941b8bd2dd9ebdcbe0c066370c
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-15 17:39:09 -0800

    WIP. shadcn. update HoverCardContent animation classes

--- src/components/ui/shadcn/hover-card.tsx
+++ src/components/ui/shadcn/hover-card.tsx
@@ -1,30 +1,16 @@
 import { forwardRef, ElementRef, ComponentPropsWithoutRef } from "react";
 import * as Prim from "@radix-ui/react-hover-card";
+import { popupAnimatonClasses } from "./shared";
 import { cn } from "@/utils";
 
 const HoverCard = Prim.Root;
 const HoverCardTrigger = Prim.Trigger;
 
-const HoverCardContentClasses = "\
+const HoverCardContentClasses = `${"\
 z-50 p-4 w-64 \
-\
-text-popover-foreground \
-bg-popover \
-\
-data-[state=open]:animate-in \
-data-[state=open]:fade-in-0 \
-data-[state=open]:zoom-in-95 \
-\
-data-[state=closed]:animate-out \
-data-[state=closed]:fade-out-0 \
-data-[state=closed]:zoom-out-95 \
-\
-data-[side=bottom]:slide-in-from-top-2 \
-data-[side=left]:slide-in-from-right-2 \
-data-[side=right]:slide-in-from-left-2 \
-data-[side=top]:slide-in-from-bottom-2 \
-\
-border rounded-md shadow-md outline-none";
+text-popover-foreground bg-popover \
+border rounded-md shadow-md outline-none \
+"} ${popupAnimatonClasses}`;
 
 const HoverCardContent = forwardRef<ElementRef<typeof Prim.Content>, ComponentPropsWithoutRef<typeof Prim.Content>>(
     ({ className, align = "center", sideOffset = 4, ...rest }, ref) => (

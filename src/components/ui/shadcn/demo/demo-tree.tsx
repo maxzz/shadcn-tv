428028c38ad2ffc8e9b56c59a5f8a103d4c3408f
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-17 17:29:44 -0800

    WIP. shadcn. Tree. kbd support. 2. move focus handle to tree

--- src/components/ui/shadcn/demo/demo-tree.tsx
+++ src/components/ui/shadcn/demo/demo-tree.tsx
@@ -75,16 +75,15 @@ export function DemoTree() {
     const [content, setContent] = useState(() => findTreeItemById(initialItemId, data)?.name || "No content selected");
     return (
         <div className="m-0.5 min-h-full flex">
-            <div className={classNames("border-[1px] rounded-l-md", inputFocusClasses)} tabIndex={-1}>
-                <Tree
-                    data={data}
-                    className={`shrink-0 w-[230px] h-[460px] ${"border-[1px] rounded-l-md"} ${inputFocusClasses}`}
-                    initialSlelectedItemId={initialItemId}
-                    onSelectChange={(item) => setContent(item?.name ?? "")}
-                    iconFolder={IconFolder}
-                    iconItem={IconWorkflow}
-                />
-            </div>
+            <Tree
+                data={data}
+                className={`shrink-0 w-[230px] h-[460px] border-[1px] rounded-l-md ${inputFocusClasses}`}
+                initialSlelectedItemId={initialItemId}
+                onSelectChange={(item) => setContent(item?.name ?? "")}
+                iconFolder={IconFolder}
+                iconItem={IconWorkflow}
+            />
+
             <div className={classNames("flex-1 px-2 py-1 border-[1px] border-l-0 rounded-r-md z-10", inputFocusClasses)} tabIndex={0}>
                 {content}
             </div>

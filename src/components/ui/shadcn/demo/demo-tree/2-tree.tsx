d5e38c9aeb17fe8ed349907b2d3cf4e7c247e304
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-23 15:34:46 -0800

    WIP. shadcn. new Tree. memo does not help for old tree. 3. reduced rerenders

--- src/components/ui/shadcn/demo/demo-tree/2-tree.tsx
+++ src/components/ui/shadcn/demo/demo-tree/2-tree.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Workflow as IconWorkflow, Folder as IconFolder } from "lucide-react";
 import { classNames } from "@/utils";
 import { inputFocusClasses } from "@/components/ui/shared-styles";
@@ -9,16 +9,31 @@ const initialItemId = "6.1.2";
 
 export function DemoTree() {
     const [content, setContent] = useState(() => findTreeItemById(data, initialItemId)?.name || "No content selected");
-    return (
-        <div className="m-0.5 min-h-full flex">
-            <Tree
+
+    const TreeMemo = useMemo(
+        () => {
+            return <Tree
                 data={data}
                 className={`shrink-0 w-[230px] h-[460px] border-[1px] rounded-l-md ${inputFocusClasses}`}
                 initialSelectedItemId={initialItemId}
                 onSelectChange={(item) => setContent(item?.name ?? "")}
                 IconForFolder={IconFolder}
                 IconForItem={IconWorkflow}
-            />
+            />;
+        }, []);
+
+    return (
+        <div className="m-0.5 min-h-full flex">
+            {/* <Tree
+                data={data}
+                className={`shrink-0 w-[230px] h-[460px] border-[1px] rounded-l-md ${inputFocusClasses}`}
+                initialSelectedItemId={initialItemId}
+                onSelectChange={(item) => setContent(item?.name ?? "")}
+                IconForFolder={IconFolder}
+                IconForItem={IconWorkflow}
+            /> */}
+
+            {TreeMemo}
 
             <div className={classNames("flex-1 px-2 py-1 border-[1px] border-l-0 rounded-r-md z-10", inputFocusClasses)} tabIndex={0}>
                 {content}

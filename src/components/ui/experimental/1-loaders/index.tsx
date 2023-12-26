f2d7cf44ec3d39a704b2521d3e39fbc0f7779a8e
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-26 15:01:09 -0800

    WIP. dots-ring. switch from vmin to percents (kind of)

--- src/components/ui/experimental/1-loaders/index.tsx
+++ src/components/ui/experimental/1-loaders/index.tsx
@@ -8,7 +8,7 @@ export function LoadersTest({ className, ...rest }: HTMLAttributes<HTMLDivElemen
             <LoaderP11 className="w-6 h-6 [--hue1:53] [--hue2:253]" />
             <LoaderP12 className="w-6 h-6 [--hue2:253]" />
             <LoaderP13 className="w-6 h-6 [--hue2:153]" />
-            <LoaderDotsRing />
+            {/* <LoaderDotsRing /> */}
         </div>
     );
 }
--- src/components/ui/loaders/dots-ring/index.tsx
+++ src/components/ui/loaders/dots-ring/index.tsx
@@ -1,11 +1,11 @@
-//import "./dots-ring.css";
 import styles from "./dots-ring.module.css";
 
 console.log("styles", styles);
 
+
 export function LoaderDotsRing() {
     return (
-        <div className="">
+        <div className={`qw-64 qh-64 ${styles["dots-ring"]}`}>
             <input className={styles.control} type="checkbox" />
             <div className={styles.bg} />
             <div className={styles.content}>

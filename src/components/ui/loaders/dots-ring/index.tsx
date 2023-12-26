0b1e4526051e8d899680d2c48bafc8a0fcf36fd8
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-26 14:25:06 -0800

    WIP. dots-ring. remove global classes

--- src/components/ui/loaders/dots-ring/index.tsx
+++ src/components/ui/loaders/dots-ring/index.tsx
@@ -1,9 +1,12 @@
 import "./dots-ring.css"; // https://codepen.io/josetxu/pen/gOEYMyQ
+import styles from "./dots-ring.module.css";
+
+console.log("styles", styles);
 
 export function LoaderDotsRing() {
     return (
         <div className="">
-            <input type="checkbox" />
+            <input className="control" type="checkbox" />
             <div className="bg" />
             <div className="content">
                 <div className="dots">

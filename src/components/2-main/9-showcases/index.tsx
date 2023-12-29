b758c8ce1ca927221914119081db57be8152bcf1
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-28 16:55:11 -0800

    WIP. fix accordion customization

--- src/components/2-main/9-showcases/index.tsx
+++ src/components/2-main/9-showcases/index.tsx
@@ -3,12 +3,12 @@ import { Showcase, initialCase } from "./cases";
 
 export function Showcases({ cases }: { cases: Showcase[]; }) {
     return (
-        <A.Accordion type="single" collapsible className="w-full" defaultValue={initialCase}>
+        <A.Accordion className="w-full" type="single" collapsible defaultValue={initialCase}>
 
             {cases.map((c) => (
-                <A.AccordionItem value={c.id} key={c.id}>
+                <A.AccordionItem className="border-b-0" value={c.id} key={c.id}>
                     <A.AccordionTrigger>{c.name}</A.AccordionTrigger>
-                    <A.AccordionContent>{c.component}</A.AccordionContent>
+                    <A.AccordionContent className="pt-4 border-t border-b" >{c.component}</A.AccordionContent>
                 </A.AccordionItem>
             ))}
 

e862280f3aff75ec0861ffacb27816a104e30eae
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-30 17:42:10 -0800

    WIP. color-picker. switch to local samples test images

--- src/components/ui/shadcn/gradient-color-picker/tabs/3-image.tsx
+++ src/components/ui/shadcn/gradient-color-picker/tabs/3-image.tsx
@@ -1,17 +1,20 @@
 import { TabsContent } from '../../tabs';
 import { SelectButton } from '../select-button';
 
-import img1 from '@/assets/test-images/1,beautiful-blue-004.jpg';
-import img2 from '@/assets/test-images/2,pretty-in-pink-005.jpg';
-import img3 from '@/assets/test-images/3,vibrant-vista-023.jpg';
-import img4 from '@/assets/test-images/4,deep-dusk-009.jpg';
-
+import img1 from '@/assets/test-images/samples/1,beautiful-blue-001.jpg';
+import img2 from '@/assets/test-images/samples/2,pretty-in-pink-005.jpg';
+import img3 from '@/assets/test-images/samples/3,vibrant-vista-004.jpg';
+import img4 from '@/assets/test-images/samples/4,deep-dusk-003.jpg';
 const images = [
-    'url(https://gradient.page/samples/beautiful-blue/beautiful-blue-001.jpg)',
-    'url(https://gradient.page/samples/pretty-in-pink/pretty-in-pink-005.jpg)',
-    'url(https://gradient.page/samples/vibrant-vista/vibrant-vista-004.jpg)',
-    'url(https://gradient.page/samples/deep-dusk/deep-dusk-003.jpg)',
+    `url(${img1})`,
+    `url(${img2})`,
+    `url(${img3})`,
+    `url(${img4})`,
 ];
+// import img1 from '@/assets/test-images/1,beautiful-blue-004.jpg';
+// import img2 from '@/assets/test-images/2,pretty-in-pink-005.jpg';
+// import img3 from '@/assets/test-images/3,vibrant-vista-023.jpg';
+// import img4 from '@/assets/test-images/4,deep-dusk-009.jpg';
 // const images = [
 //     `url(${img1})`,
 //     `url(${img2})`,
@@ -23,6 +26,12 @@ const images = [
 //     'url(https://images.unsplash.com/photo-1691226099773-b13a89a1d167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
 //     'url(https://images.unsplash.com/photo-1688822863426-8c5f9b257090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
 //     'url(https://images.unsplash.com/photo-1691225850735-6e4e51834cad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
+// ];
+// const images = [
+//     'url(https://gradient.page/samples/beautiful-blue/beautiful-blue-001.jpg)',
+//     'url(https://gradient.page/samples/pretty-in-pink/pretty-in-pink-005.jpg)',
+//     'url(https://gradient.page/samples/vibrant-vista/vibrant-vista-004.jpg)',
+//     'url(https://gradient.page/samples/deep-dusk/deep-dusk-003.jpg)',
 // ];
 
 export function TabImage({ background, setBackground }: { background: string; setBackground: (background: string) => void; }) {

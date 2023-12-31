95769f106a652bb409c7f451c9fbcd7638ad95bc
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-30 17:29:07 -0800

    WIP. color-picker. switch to samples test images

--- src/components/ui/shadcn/gradient-color-picker/tabs/3-image.tsx
+++ src/components/ui/shadcn/gradient-color-picker/tabs/3-image.tsx
@@ -7,14 +7,20 @@ import img3 from '@/assets/test-images/3,vibrant-vista-023.jpg';
 import img4 from '@/assets/test-images/4,deep-dusk-009.jpg';
 
 const images = [
-    `url(${img1})`,
-    `url(${img2})`,
-    `url(${img3})`,
-    `url(${img4})`,
+    'url(https://gradient.page/samples/beautiful-blue/beautiful-blue-001.jpg)',
+    'url(https://gradient.page/samples/pretty-in-pink/pretty-in-pink-005.jpg)',
+    'url(https://gradient.page/samples/vibrant-vista/vibrant-vista-004.jpg)',
+    'url(https://gradient.page/samples/deep-dusk/deep-dusk-003.jpg)',
 ];
 // const images = [
+//     `url(${img1})`,
+//     `url(${img2})`,
+//     `url(${img3})`,
+//     `url(${img4})`,
+// ];
+// const images = [
 //     'url(https://images.unsplash.com/photo-1691200099282-16fd34790ade?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
-//     'url(https://images.unsplash.com/photo-1691226099773-b13a89a1d167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90',
+//     'url(https://images.unsplash.com/photo-1691226099773-b13a89a1d167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
 //     'url(https://images.unsplash.com/photo-1688822863426-8c5f9b257090?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
 //     'url(https://images.unsplash.com/photo-1691225850735-6e4e51834cad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2532&q=90)',
 // ];

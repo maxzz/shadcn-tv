ac95728419f215739ab5c7bb964f84c29d71674a
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-31 17:32:50 -0800

    WIP. radial loader. clean up styles

--- src/components/ui/loaders/radial/index.tsx
+++ src/components/ui/loaders/radial/index.tsx
@@ -2,7 +2,7 @@ import "./radial.css";
 
 export function RadialLoader() {
     return (
-        <div className="flexBox">
+        <div className="1relative 1w-24 1h-24 flex items-center justify-center">
             <div id="preloader">
                 <div className="a">
                     <div className="b"></div>

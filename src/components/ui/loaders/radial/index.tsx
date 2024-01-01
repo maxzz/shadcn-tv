5fc1c5e1f75a9a772074458263bb63c1915fd333
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-31 18:21:54 -0800

    WIP. radial loader. optimize tailwind classes

--- src/components/ui/loaders/radial/index.tsx
+++ src/components/ui/loaders/radial/index.tsx
@@ -2,11 +2,13 @@ import styles from "./radial.module.css";
 
 export function RadialLoader() {
     return (
-        <div className="m-8 text-green-500 flex items-center justify-center">
-            <div className="relative w-32 h-32 border-current border-[14px] rounded-full" id="preloader">
-                <div className={`${styles.a} m-1 absolute inset-0 bg-transparent border-current border-[8px] border-t-transparent border-b-transparent rounded-full`} />
-                <div className={`${styles.b} m-4 absolute inset-0 bg-transparent border-current border-[20px] border-l-transparent border-r-transparent rounded-full`} />
+        <div className="p-4 text-green-950 flex items-center justify-center">
+
+            <div className="relative w-32 h-32 border-current border-[14px] rounded-full">
+                <div className={`${styles.a} m-1 absolute inset-0 border-current border-[8px] border-y-transparent rounded-full`} />
+                <div className={`${styles.b} m-4 absolute inset-0 border-current border-[20px] border-x-transparent rounded-full`} />
             </div>
+           
         </div>
     );
 }

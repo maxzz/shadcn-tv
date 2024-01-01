9648ddd719967bff09c683a52c5ee5cda0ce2c4f
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-31 18:09:58 -0800

    WIP. radial loader. switch to tailwind

--- src/components/ui/loaders/radial/index.tsx
+++ src/components/ui/loaders/radial/index.tsx
@@ -2,12 +2,12 @@ import "./radial.css";
 
 export function RadialLoader() {
     return (
-        <div className="1relative 1w-24 1h-24 flex items-center justify-center">
-            <div id="preloader">
-                <div className="a">
-                    <div className="b"></div>
-                </div>
+        <div className="text-green-500">
+            <div className="relative w-32 h-32 border-current border-[14px] rounded-full" id="preloader">
+                <div className="a m-1 absolute inset-0 bg-transparent border-current border-[8px] border-t-transparent border-b-transparent rounded-full" />
+                <div className="b m-4 absolute inset-0 bg-transparent border-current border-[20px] border-l-transparent border-r-transparent rounded-full" />
             </div>
         </div>
     );
 }
+//flex items-center justify-center

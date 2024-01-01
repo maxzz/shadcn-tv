c37fe4e4fa30756cac3662109b27ad556a095991
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-31 18:15:16 -0800

    WIP. radial loader. switch to module css

--- src/components/ui/loaders/radial/index.tsx
+++ src/components/ui/loaders/radial/index.tsx
@@ -1,13 +1,12 @@
-import "./radial.css";
+import styles from "./radial.module.css";
 
 export function RadialLoader() {
     return (
-        <div className="text-green-500">
+        <div className="m-8 text-green-500 flex items-center justify-center">
             <div className="relative w-32 h-32 border-current border-[14px] rounded-full" id="preloader">
-                <div className="a m-1 absolute inset-0 bg-transparent border-current border-[8px] border-t-transparent border-b-transparent rounded-full" />
-                <div className="b m-4 absolute inset-0 bg-transparent border-current border-[20px] border-l-transparent border-r-transparent rounded-full" />
+                <div className={`${styles.a} m-1 absolute inset-0 bg-transparent border-current border-[8px] border-t-transparent border-b-transparent rounded-full`} />
+                <div className={`${styles.b} m-4 absolute inset-0 bg-transparent border-current border-[20px] border-l-transparent border-r-transparent rounded-full`} />
             </div>
         </div>
     );
 }
-//flex items-center justify-center

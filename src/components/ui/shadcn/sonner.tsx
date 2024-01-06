64aa19f591f14b10c5a2cdd7d3c406b1933c0a50
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-05 18:21:45 -0800

    WIP. sonner. code review. lib next-themes is optional

--- src/components/ui/shadcn/sonner.tsx
+++ src/components/ui/shadcn/sonner.tsx
@@ -1,32 +1,34 @@
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const toastClasses = "\
 group toast \
-group-[.toaster]:bg-background \
 group-[.toaster]:text-foreground \
+group-[.toaster]:bg-background \
 group-[.toaster]:border-border \
 group-[.toaster]:shadow-lg \
 ";
 const descriptionClasses = "group-[.toast]:text-muted-foreground";
 const actionButtonClasses = "\
-group-[.toast]:bg-primary \
 group-[.toast]:text-primary-foreground \
+group-[.toast]:bg-primary \
 ";
 const cancelButtonClasses = "\
-group-[.toast]:bg-muted \
 group-[.toast]:text-muted-foreground \
+group-[.toast]:bg-muted \
 ";
 
 /**
+ * To use toast() from anywhere, even wo/ hooks context, simply add <Toaster /> into <App> component.
  * https://sonner.emilkowal.ski - docs
  * https://github.com/emilkowalski/sonner
  * https://github.com/pacocoursey/next-themes
  */
 export function Toaster(props: ToasterProps) {
-    const { theme = "system" } = useTheme();
+    // const { theme = "system" } = useTheme();
+    const theme = "system";
     return (
         <Sonner
             theme={theme as ToasterProps["theme"]}

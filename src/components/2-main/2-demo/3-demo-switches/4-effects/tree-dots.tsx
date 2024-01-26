6ea99bcfbd528af2ddcbe7499194a151820a4868
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-25 18:51:35 -0800

    WIP. button effects. 3dots. code clean up

--- src/components/2-main/2-demo/3-demo-switches/4-effects/tree-dots.tsx
+++ src/components/2-main/2-demo/3-demo-switches/4-effects/tree-dots.tsx
@@ -4,126 +4,14 @@
 // https://github.com/emilkowalski/ui-snippets/blob/master/components/SnippetsGrid/SnippetsGrid.js
 // https://github.com/emilkowalski/ui-snippets/blob/master/components/SnippetsGrid/SnippetsGridItem.js
 
-// import { styled, keyframes } from 'stitches.config.js';
 import styles from "./tree-dots.module.css";
 
 export function ThreeDotsLoader() {
     return (
-        <div className={styles.exampleClass}>
+        <div className={styles.wrapper}>
             <span></span>
             <span></span>
             <span></span>
         </div>
     );
 }
-/*
-const Component = () => {
-    return (
-        <Wrapper>
-            <span></span>
-            <span></span>
-            <span></span>
-        </Wrapper>
-    );
-};
-
-const flashing = keyframes({
-    '0%': {
-        opacity: 0.2
-    },
-    '20%': {
-        opacity: 1
-    },
-    '100%': {
-        opacity: 0.2
-    }
-});
-
-const Wrapper = styled('div', {
-    span: {
-        width: '8px',
-        height: '8px',
-        borderRadius: '50%',
-        backgroundColor: '$white',
-        animation: `${flashing} 1.4s infinite linear`,
-        margin: '0 4px',
-        display: 'inline-block',
-
-        '&:nth-child(2)': {
-            'animation-delay': '.2s'
-        },
-
-        '&:nth-child(3)': {
-            'animation-delay': '.4s'
-        }
-    }
-});
-
-const cssStyling = `
-.exampleClass span {
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  background-color: white;
-  animation: flashing 1.4s infinite linear;
-  margin: 0 4px;
-  display: inline-block;
-}
-
-.exampleClass span:nth-child(2) {
-  animation-delay: 0.2s;
-}
-
-.exampleClass span:nth-child(3) {
-  animation-delay: 0.4s;
-}
-
-@keyframes flashing {
-  0% {
-    opacity: 0.2;
-  }
-  20% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 0.2;
-  }
-}
-`;
-
-const scssStyling = `
-.exampleClass {
-  span {
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background-color: white;
-    animation: flashing 1.4s infinite linear;
-    margin: 0 4px;
-    display: inline-block;
-
-    &:nth-child(2) {
-      animation-delay: 0.2s;
-    }
-
-    &:nth-child(3) {
-      animation-delay: 0.4s;
-    }
-  }
-}
-
-@keyframes flashing {
-    0% {
-      opacity: 0.2;
-    }
-    20% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0.2;
-    }
-  }
-`;
-
-export { cssStyling, scssStyling, Component };
-*/

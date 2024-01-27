63e5834f4b818e53b56b85fe9b1cb9a494825ea4
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-27 15:58:07 -0800

    WIP. Spinner7. done for now

--- src/components/ui/loaders/spinner7/index.tsx
+++ src/components/ui/loaders/spinner7/index.tsx
@@ -1,2 +1,3 @@
-// https://codepen.io/MhmdNadyMhmd/pen/dgOmYm
+// https://codepen.io/MhmdNadyMhmd/pen/dgOmYm 'CSS3 Spinning Preloader'
+
 export * from "./spinner7";
--- src/components/ui/loaders/spinner7/spinner7.tsx
+++ src/components/ui/loaders/spinner7/spinner7.tsx
@@ -2,11 +2,9 @@ import styles from "./spinner7.module.css";
 
 export function Spinner7() {
     return (
-        <div>
-            <div className={`${styles["loader"]}`}>
-                Loading
-                <span />
-            </div>
+        <div className={`relative w-[150px] h-[150px] ${styles["loader"]}`}>
+            Loading
+            <span />
         </div>
     );
 }

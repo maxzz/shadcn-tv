204d8c495565c654449bb9ad9aa40d6b2e9f2266
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-28 19:03:36 -0800

    WIP. ColorPicker. connect add SolidColorPicker multiple colors

--- src/components/2-main/2-demo/70-pickes/1-color-picker/index.tsx
+++ src/components/2-main/2-demo/70-pickes/1-color-picker/index.tsx
@@ -3,14 +3,18 @@ import { SolidColorPicker } from "./color-picker";
 import { color, type HslaColor } from "@uiw/react-color";
 
 export function SolidColorPickerDemo() {
-    const [color2, setColor2] = useState<HslaColor>(color("#ff0000").hsla);
+    const [color2, setColor2] = useState<HslaColor>(color("#217EFF").hsla);
+    const [color3, setColor3] = useState<HslaColor>(color("#FFA121").hsla);
     return (
         <div>
-            SolidColorPicker
-            <br />
-            <SolidColorPicker color={color2} onColorChange={(color2) => {
-                setColor2(color2.hsla);
-            }} />
+            <div className="flex flex-col gap-2">
+                <SolidColorPicker color={color2} onColorChange={(color2) => {
+                    setColor2(color2.hsla);
+                }} />
+                <SolidColorPicker color={color3} onColorChange={(color3) => {
+                    setColor3(color3.hsla);
+                }} />
+            </div>
         </div>
     );
 }

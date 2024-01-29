2855a5823d750a466744eef9a1c65ac252438644
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-28 19:00:11 -0800

    WIP. ColorPicker. connect SolidColorPicker demo

--- src/components/2-main/2-demo/70-pickes/1-color-picker/color-picker.tsx
+++ src/components/2-main/2-demo/70-pickes/1-color-picker/color-picker.tsx
@@ -1,25 +1,23 @@
-// https://github.com/jln13x/ui.jln.dev/blob/main/src/client/components/color-picker.tsx
-
 import { PopoverContent } from "@radix-ui/react-popover";
 import { Popover, PopoverTrigger } from "@/components/ui/shadcn/popover";
 import { hslaToHsva, hsvaToHex, type ColorResult, type HslColor } from "@uiw/react-color";
 import ChromeColorPicker from "@uiw/react-color-chrome";
 
+// https://github.com/jln13x/ui.jln.dev/blob/main/src/client/components/color-picker.tsx
+
 export function SolidColorPicker({ color, onColorChange }: { color: HslColor; onColorChange: (color: ColorResult) => void; }) {
     const hsva = hslaToHsva({ ...color, a: 1, });
     return (
         <Popover>
+
             <PopoverTrigger asChild>
-                <button
-                    className="size-6 flex-shrink-0 rounded border"
-                    style={{
-                        backgroundColor: hsvaToHex(hsva),
-                    }}
-                ></button>
+                <button className="size-6 flex-shrink-0 rounded border" style={{ backgroundColor: hsvaToHex(hsva), }} />
             </PopoverTrigger>
+
             <PopoverContent className="isolate z-50">
                 <ChromeColorPicker color={hsva} onChange={onColorChange} />
             </PopoverContent>
+
         </Popover>
     );
 }
--- src/components/2-main/2-demo/70-pickes/1-color-picker/index.tsx
+++ src/components/2-main/2-demo/70-pickes/1-color-picker/index.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import { SolidColorPicker } from "./color-picker";
+import { color, type HslaColor } from "@uiw/react-color";
+
 export function SolidColorPickerDemo() {
+    const [color2, setColor2] = useState<HslaColor>(color("#ff0000").hsla);
     return (
-        <div>SolidColorPicker</div>
+        <div>
+            SolidColorPicker
+            <br />
+            <SolidColorPicker color={color2} onColorChange={(color2) => {
+                setColor2(color2.hsla);
+            }} />
+        </div>
     );
 }

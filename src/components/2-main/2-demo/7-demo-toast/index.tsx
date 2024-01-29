2c10ae7d9c7240571dd7add8d0cd9b885adbc52c
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-28 16:25:42 -0800

    WIP. toaster. initial commit

---
+++ src/components/2-main/2-demo/7-demo-toast/index.tsx
@@ -0,0 +1,3 @@
+// Inspired by react-hot-toast library
+// https://github.com/jln13x/ui.jln.dev/blob/main/src/client/components/customizable/use-toast.ts
+// https://github.com/jln13x/ui.jln.dev/blob/main/src/client/components/customizable/toaster.tsx

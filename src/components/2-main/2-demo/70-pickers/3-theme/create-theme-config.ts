5d6cbcdae638be5be8bb139b88c7bfab1acae550
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-29 17:44:50 -0800

    WIP. theme. code review

--- src/components/2-main/2-demo/70-pickers/3-theme/create-theme-config.ts
+++ src/components/2-main/2-demo/70-pickers/3-theme/create-theme-config.ts
@@ -9,10 +9,7 @@ extend([a11yPlugin, harmoniesPlugin]);
 const faker = {
     number: {
         int: (options: { min: number; max: number; }) => {
-            return (
-                Math.floor(Math.random() * (options.max - options.min + 1)) +
-                options.min
-            );
+            return Math.floor(Math.random() * (options.max - options.min + 1)) + options.min;
         },
         float: (options: { min: number; max: number; }) => {
             return Math.random() * (options.max - options.min) + options.min;
@@ -24,116 +21,88 @@ const faker = {
         },
     },
     helpers: {
-        arrayElement: <T extends readonly unknown[] | Array<unknown>>(
-            arr: T,
-        ): T[number] => {
+        arrayElement: <T extends readonly unknown[] | Array<unknown>>(arr: T): T[number] => {
             return arr[Math.floor(Math.random() * arr.length)];
         },
     },
 };
 
-const createPrimaryColor = (): Hsl => {
-    return {
-        h: faker.number.int({ min: 0, max: 360 }),
-        s: faker.number.int({ min: 0, max: 100 }),
-        l: faker.number.int({ min: 10, max: 90 }),
-    };
-};
-
-const createContrast = (color: Colord) => {
-    const isLight = color.isLight();
-    let opposite = color;
-
-    let i = 0;
-    while (opposite.contrast(color) < 6) {
-        opposite = isLight ? opposite.darken(0.2) : opposite.lighten(0.2);
-        if (i++ > 10) break;
-    }
-    return opposite;
-};
-
-const colordToHsl = (color: Colord): Hsl => {
+function colordToHsl(color: Colord): Hsl {
     const hsla = color.toHsl();
     return {
         h: hsla.h,
         s: hsla.s,
         l: hsla.l,
     };
-};
+}
 
-const createBackgroundLight = (hue: number): Hsl => {
+function createPrimaryColor(): Hsl {
+    return {
+        h: faker.number.int({ min: 0, max: 360 }),
+        s: faker.number.int({ min: 0, max: 100 }),
+        l: faker.number.int({ min: 10, max: 90 }),
+    };
+}
+
+function createContrast(color: Colord) {
+    const isLight = color.isLight();
+    let opposite = color;
+    let i = 0;
+    while (opposite.contrast(color) < 6) {
+        opposite = isLight ? opposite.darken(0.2) : opposite.lighten(0.2);
+        if (i++ > 10) {
+            break;
+        }
+    }
+    return opposite;
+}
+
+function createBackgroundLight(hue: number): Hsl {
     return {
         h: hue,
         s: faker.number.int({ min: 30, max: 70 }),
         l: faker.number.int({ min: 98, max: 100 }),
     };
-};
+}
 
-const createBackgroundDark = (hue: number): Hsl => {
+function createBackgroundDark(hue: number): Hsl {
     return {
         h: hue,
         s: faker.number.int({ min: 30, max: 60 }),
         l: faker.number.int({ min: 0, max: 4 }),
     };
-};
+}
 
-const createForegroundLight = (hue: number): Hsl => {
+function createForegroundLight(hue: number): Hsl {
     return {
         h: hue,
-        s: faker.number.int({
-            min: 50,
-            max: 80,
-        }),
-        l: faker.number.int({
-            min: 0,
-            max: 5,
-        }),
+        s: faker.number.int({ min: 50, max: 80, }),
+        l: faker.number.int({ min: 0, max: 5, }),
     };
-};
+}
 
-const createForegroundDark = (hue: number): Hsl => {
+function createForegroundDark(hue: number): Hsl {
     return {
         h: hue,
-        s: faker.number.int({
-            min: 10,
-            max: 40,
-        }),
-        l: faker.number.int({
-            min: 97,
-            max: 100,
-        }),
+        s: faker.number.int({ min: 10, max: 40, }),
+        l: faker.number.int({ min: 97, max: 100, }),
     };
-};
+}
 
-const createDestructive = () => {
+function createDestructive() {
     return new Colord({
-        h: faker.number.int({
-            min: 0,
-            max: 22,
-        }),
-        s: faker.number.int({
-            min: 80,
-            max: 100,
-        }),
-        l: faker.number.int({
-            min: 20,
-            max: 45,
-        }),
+        h: faker.number.int({ min: 0, max: 22, }),
+        s: faker.number.int({ min: 80, max: 100, }),
+        l: faker.number.int({ min: 20, max: 45, }),
     });
-};
+}
 
 const modes = ["complementary", "triadic", "analogous", "slick"] as const;
 
-const createColorHarmony = (
-    primary: Colord,
-    mode: (typeof modes)[number],
-    shouldMatch: boolean,
-    isDark?: boolean,
-) => {
+function createColorHarmony(primary: Colord, mode: (typeof modes)[number], shouldMatch: boolean, isDark?: boolean) {
     if (mode === "triadic") {
         const [, secondary, accent] = primary.harmonies(mode);
         if (!secondary || !accent) throw new Error("Failed to create harmony");
-
         return {
             secondary,
             accent,
@@ -143,7 +112,6 @@ const createColorHarmony = (
     if (mode === "complementary") {
         const [, secondary] = primary.harmonies(mode);
         if (!secondary) throw new Error("Failed to create harmony");
-
         return {
             secondary,
             accent: secondary,
@@ -153,7 +121,6 @@ const createColorHarmony = (
     if (mode === "analogous") {
         const [secondary, , accent] = primary.harmonies(mode);
         if (!secondary || !accent) throw new Error("Failed to create harmony");
-
         return {
             secondary,
             accent,
@@ -201,9 +168,9 @@ const createColorHarmony = (
     }
 
     throw new Error("Invalid mode");
-};
+}
 
-export const createThemeConfig = (primaryColor?: Hsl) => {
+export function createThemeConfig(primaryColor?: Hsl) {
     const primaryBase = new Colord(primaryColor ?? createPrimaryColor());
 
     const primaryLightColord = primaryBase;
@@ -211,9 +178,7 @@ export const createThemeConfig = (primaryColor?: Hsl) => {
 
     const primaryLight = colordToHsl(primaryLightColord);
     const primaryDark = colordToHsl(primaryDarkColord);
-    const primaryLightForeground = colordToHsl(
-        createContrast(primaryLightColord),
-    );
+    const primaryLightForeground = colordToHsl(createContrast(primaryLightColord));
     const primaryDarkForeground = colordToHsl(createContrast(primaryDarkColord));
 
     const backgroundDark = createBackgroundDark(primaryBase.hue());
@@ -224,140 +189,72 @@ export const createThemeConfig = (primaryColor?: Hsl) => {
 
     const cardBoolean = faker.datatype.boolean();
 
-    const cardLight = cardBoolean
-        ? colordToHsl(new Colord(backgroundLight).darken(0.01))
-        : backgroundLight;
-    const cardDark = cardBoolean
-        ? colordToHsl(new Colord(backgroundDark).lighten(0.01))
-        : backgroundDark;
+    const cardLight = cardBoolean ? colordToHsl(new Colord(backgroundLight).darken(0.01)) : backgroundLight;
+    const cardDark = cardBoolean ? colordToHsl(new Colord(backgroundDark).lighten(0.01)) : backgroundDark;
 
-    const cardLightForeground = cardBoolean
-        ? colordToHsl(new Colord(foregroundLight).darken(0.01))
-        : foregroundLight;
-
-    const cardDarkForeground = cardBoolean
-        ? colordToHsl(new Colord(foregroundDark).lighten(0.01))
-        : foregroundDark;
+    const cardLightForeground = cardBoolean ? colordToHsl(new Colord(foregroundLight).darken(0.01)) : foregroundLight;
+    const cardDarkForeground = cardBoolean ? colordToHsl(new Colord(foregroundDark).lighten(0.01)) : foregroundDark;
 
     const popoverBoolean = faker.datatype.boolean();
 
     const popoverLight = popoverBoolean ? cardLight : backgroundLight;
     const popoverDark = popoverBoolean ? cardDark : backgroundDark;
 
-    const popoverLightForeground = popoverBoolean
-        ? cardLightForeground
-        : foregroundLight;
-
-    const popoverDarkForeground = popoverBoolean
-        ? cardDarkForeground
-        : foregroundDark;
+    const popoverLightForeground = popoverBoolean ? cardLightForeground : foregroundLight;
+    const popoverDarkForeground = popoverBoolean ? cardDarkForeground : foregroundDark;
 
     const harmonyMode = faker.helpers.arrayElement(modes);
 
     // Whether the secondary and accent colors should match if mode is "slick"
     const shouldMatch = faker.datatype.boolean();
 
-    const lightHarmonies = createColorHarmony(
-        primaryLightColord,
-        harmonyMode,
-        shouldMatch,
-        false,
-    );
+    const lightHarmonies = createColorHarmony(primaryLightColord, harmonyMode, shouldMatch, false);
 
     const secondaryLight = colordToHsl(lightHarmonies.secondary);
-    const secondaryLightForeground = colordToHsl(
-        createContrast(lightHarmonies.secondary),
-    );
+    const secondaryLightForeground = colordToHsl(createContrast(lightHarmonies.secondary));
 
     const accentLight = colordToHsl(lightHarmonies.accent);
-    const accentLightForeground = colordToHsl(
-        createContrast(lightHarmonies.accent),
-    );
+    const accentLightForeground = colordToHsl(createContrast(lightHarmonies.accent));
 
-    const darkHarmonies = createColorHarmony(
-        primaryDarkColord,
-        harmonyMode,
-        shouldMatch,
-        true,
-    );
+    const darkHarmonies = createColorHarmony(primaryDarkColord, harmonyMode, shouldMatch, true);
 
     const secondaryDark = colordToHsl(darkHarmonies.secondary);
-    const secondaryDarkForeground = colordToHsl(
-        createContrast(darkHarmonies.secondary),
-    );
+    const secondaryDarkForeground = colordToHsl(createContrast(darkHarmonies.secondary));
 
     const accentDark = colordToHsl(darkHarmonies.accent);
-    const accentDarkForeground = colordToHsl(
-        createContrast(darkHarmonies.accent),
-    );
+    const accentDarkForeground = colordToHsl(createContrast(darkHarmonies.accent));
 
     const destructiveBase = createDestructive();
+
     const destructiveLight = colordToHsl(destructiveBase);
+    const destructiveDark = { h: destructiveLight.h, s: destructiveLight.s, l: faker.number.int({ min: 45, max: 60 }), };
 
-    const destructiveDark = {
-        h: destructiveLight.h,
-        s: destructiveLight.s,
-        l: faker.number.int({ min: 45, max: 60 }),
-    };
-
-    const destructiveLightForeground = colordToHsl(
-        createContrast(destructiveBase),
-    );
-
-    const destructiveDarkForeground = colordToHsl(
-        createContrast(new Colord(destructiveDark)),
-    );
+    const destructiveLightForeground = colordToHsl(createContrast(destructiveBase));
+    const destructiveDarkForeground = colordToHsl(createContrast(new Colord(destructiveDark)));
 
     const mutedBaseDeviations = {
         s: faker.number.int({ min: 5, max: 40 }),
         l: faker.number.int({ min: 0, max: 10 }),
     };
 
-    const mutedLight = {
-        h: secondaryLight.h,
-        s: mutedBaseDeviations.s,
-        l: 85 + mutedBaseDeviations.l,
-    };
-
-    const mutedDark = {
-        h: secondaryDark.h,
-        s: mutedBaseDeviations.s,
-        l: 15 - mutedBaseDeviations.l,
-    };
+    const mutedLight = { h: secondaryLight.h, s: mutedBaseDeviations.s, l: 85 + mutedBaseDeviations.l, };
+    const mutedDark = { h: secondaryDark.h, s: mutedBaseDeviations.s, l: 15 - mutedBaseDeviations.l, };
 
     const mutedForegroundBaseDeviations = {
         s: faker.number.int({ min: 0, max: 15 }),
         l: faker.number.int({ min: 0, max: 15 }),
     };
 
-    const mutedForegroundLight = {
-        h: mutedLight.h,
-        s: mutedForegroundBaseDeviations.s,
-        l: 25 + mutedForegroundBaseDeviations.l,
-    };
-
-    const mutedForegroundDark = {
-        h: mutedDark.h,
-        s: mutedForegroundBaseDeviations.s,
-        l: 75 - mutedForegroundBaseDeviations.l,
-    };
+    const mutedForegroundLight = { h: mutedLight.h, s: mutedForegroundBaseDeviations.s, l: 25 + mutedForegroundBaseDeviations.l, };
+    const mutedForegroundDark = { h: mutedDark.h, s: mutedForegroundBaseDeviations.s, l: 75 - mutedForegroundBaseDeviations.l, };
 
     const inputBaseDeviations = {
         s: faker.number.int({ min: 2, max: 15 }),
         l: faker.number.int({ min: 5, max: 10 }),
     };
 
-    const borderLight = {
-        h: backgroundLight.h,
-        s: inputBaseDeviations.s,
-        l: backgroundLight.l - inputBaseDeviations.l,
-    };
-
-    const borderDark = {
-        h: backgroundDark.h,
-        s: inputBaseDeviations.s,
-        l: faker.number.int({ min: 10, max: 15 }),
-    };
+    const borderLight = { h: backgroundLight.h, s: inputBaseDeviations.s, l: backgroundLight.l - inputBaseDeviations.l, };
+    const borderDark = { h: backgroundDark.h, s: inputBaseDeviations.s, l: faker.number.int({ min: 10, max: 15 }), };
 
     return {
         light: {
@@ -403,4 +300,4 @@ export const createThemeConfig = (primaryColor?: Hsl) => {
             ring: primaryDark,
         },
     };
-};
+}

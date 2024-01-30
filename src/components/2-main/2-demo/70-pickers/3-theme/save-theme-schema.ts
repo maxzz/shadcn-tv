8750a1d42c6c10b59256479bde9f779f1c1cbb11
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-29 18:04:48 -0800

    WIP. theme. theme-customize initial commit

--- src/components/2-main/2-demo/70-pickers/3-theme/save-theme-schema.ts
+++ src/components/2-main/2-demo/70-pickers/3-theme/save-theme-schema.ts
@@ -1,3 +1,4 @@
+// 1
 // import { NonEmptyString } from "@jlns/zod";
 // import { z } from "zod";
 
@@ -7,3 +8,33 @@
 //     }),
 //     isPublic: z.boolean(),
 // });
+
+
+
+// 2
+// import { z } from "zod";
+
+// export const NonEmptyString = z.string().min(1);
+
+// export const SaveThemeSchema = z.object({
+//     name: NonEmptyString.max(50, {
+//         message: "Too long",
+//     }),
+//     isPublic: z.boolean(),
+// });
+
+
+
+// 3
+import { z } from "zod";
+
+export const nonEmptyString = (message = 'String value is required') => {
+    return z.string().min(1, { message });
+};
+
+export const SaveThemeSchema = z.object({
+    name: nonEmptyString('Too short').max(50, {
+        message: "Too long",
+    }),
+    isPublic: z.boolean(),
+});

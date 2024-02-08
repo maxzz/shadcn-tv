4e00837b4d7e4f8428e3ae86b392cd42528b2f24
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-07 17:18:36 -0800

    WIP. xarrows. add demo 3 as map

---
+++ src/components/2-main/2-demo/73-demo-xarrow/3-demo-persistent-map/index.tsx
@@ -0,0 +1,52 @@
+import { MutableRefObject, RefObject, createRef, useMemo, useRef } from "react";
+import { Xwrapper } from "react-xarrows";
+import { DemoControls } from "../3-controls";
+import { Arrow } from "../4-arrow";
+import { DraggableBox } from "./box";
+import { useSnapshot } from "valtio";
+import { appSettings } from "@/store";
+
+export function XArrowsDemo3() {
+    // const box1Ref = useRef(null);
+    // const box2Ref = useRef(null);
+
+    const { boxes } = useSnapshot(appSettings.xArrowsState);
+
+    // const refs = useRef<MutableRefObject<null>[]>([box1Ref, box2Ref]);
+
+    const itemsRef = useMemo<RefObject<HTMLDivElement>[]>(() => Array(boxes.length).fill(0).map(() => createRef()), [boxes.length]);
+
+    return (
+        <div className="h-[240px] relative bg-muted rounded overflow-hidden">
+            <DemoControls />
+
+            <Xwrapper>
+                {boxes.map(
+                    (box, idx) => (
+                        <DraggableBox
+                            ref={itemsRef[idx]}
+                            label={box.label}
+                            dragOptions={{ defaultPosition: { x: boxes[idx].x, y: boxes[idx].y } }}
+                            key={box.id}
+                        />
+                    ))
+                }
+
+                {/* <DraggableBox
+                    ref={box1Ref}
+                    label={'elem1'}
+                    dragOptions={{ defaultPosition: { x: 0, y: 0 } }}
+                />
+
+                <DraggableBox
+                    ref={box2Ref}
+                    label={'elem2'}
+                    dragOptions={{ defaultPosition: { x: 140, y: 160 } }}
+                /> */}
+
+                {/* <Arrow box1Ref={box1Ref} box2Ref={box2Ref} /> */}
+                <Arrow box1Ref={itemsRef[1]} box2Ref={itemsRef[0]} />
+            </Xwrapper>
+        </div >
+    );
+}
--- src/components/2-main/2-demo/73-demo-xarrow/index.tsx
+++ src/components/2-main/2-demo/73-demo-xarrow/index.tsx
@@ -1,1 +1,4 @@
 export * from "./1-demo-simple";
+export * from "./2-demo-persistent-array";
+export * from "./3-demo-persistent-map";
+

e8713812330e920ae69f5601a832fb1e462ee29c
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-07 18:50:03 -0800

    WIP. xarrows. demo 2 code clean up

--- src/components/2-main/2-demo/73-demo-xarrow/2-demo-persistent-array/index.tsx
+++ src/components/2-main/2-demo/73-demo-xarrow/2-demo-persistent-array/index.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject, RefObject, createRef, useMemo, useRef } from "react";
+import { RefObject, createRef, useMemo } from "react";
 import { Xwrapper } from "react-xarrows";
 import { DemoControls } from "../3-controls";
 import { Arrow } from "../4-arrow";
@@ -7,15 +7,8 @@ import { useSnapshot } from "valtio";
 import { appSettings } from "@/store";
 
 export function XArrowsDemo2() {
-    // const box1Ref = useRef(null);
-    // const box2Ref = useRef(null);
-
     const { boxes } = useSnapshot(appSettings.xArrowsState);
-
-    // const refs = useRef<MutableRefObject<null>[]>([box1Ref, box2Ref]);
-
     const itemsRef = useMemo<RefObject<HTMLDivElement>[]>(() => Array(boxes.length).fill(0).map(() => createRef()), [boxes.length]);
-
     return (
         <div className="h-[240px] relative bg-muted rounded overflow-hidden">
             <DemoControls />
@@ -32,19 +25,6 @@ export function XArrowsDemo2() {
                     ))
                 }
 
-                {/* <DraggableBox
-                    ref={box1Ref}
-                    label={'elem1'}
-                    dragOptions={{ defaultPosition: { x: 0, y: 0 } }}
-                />
-
-                <DraggableBox
-                    ref={box2Ref}
-                    label={'elem2'}
-                    dragOptions={{ defaultPosition: { x: 140, y: 160 } }}
-                /> */}
-
-                {/* <Arrow box1Ref={box1Ref} box2Ref={box2Ref} /> */}
                 <Arrow box1Ref={itemsRef[1]} box2Ref={itemsRef[0]} />
             </Xwrapper>
         </div >

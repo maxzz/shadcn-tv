a4d05891537b3a4cbccc3a108fce7ce54bafb40a
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-20 14:24:17 -0800

    WIP. svg circle loaders. fix demo container

--- src/components/ui/loaders/spinner-circles/index.tsx
+++ src/components/ui/loaders/spinner-circles/index.tsx
@@ -3,8 +3,8 @@ import css from "./circles.module.css"; // https://codepen.io/yoksel/pen/KKqeyj 
 export function SpinnerCircles() {
     return (
         <div className="p-4 text-green-950 flex items-center justify-center">
-            <div className="l-wrapper">
-                
+            <div className="relative w-64 flex items-center gap-4">
+
                 <svg
                     viewBox="0 0 120 120"
                     version="1.1"
@@ -14,7 +14,7 @@ export function SpinnerCircles() {
                     <symbol id="s--circle">
                         <circle r={10} cx={20} cy={20} />
                     </symbol>
-                    <g className={css["g-circles g-circles--v1"]}>
+                    <g className={`${css["g-circles"]} ${css["g-circles--v1"]}`}>
                         <g className={css["g--circle"]}>
                             <use xlinkHref="#s--circle" className={css["u--circle"]} />
                         </g>
@@ -60,7 +60,7 @@ export function SpinnerCircles() {
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                 >
-                    <g className={css["g-circles g-circles--v2"]}>
+                    <g className={`${css["g-circles"]} ${css["g-circles--v2"]}`}>
                         <g className={css["g--circle"]}>
                             <use xlinkHref="#s--circle" className={css["u--circle"]} />
                         </g>
@@ -105,7 +105,7 @@ export function SpinnerCircles() {
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                 >
-                    <g className={css["g-circles g-circles--v3"]}>
+                    <g className={`${css["g-circles"]} ${css["g-circles--v3"]}`}>
                         <g className={css["g--circle"]}>
                             <use xlinkHref="#s--circle" className={css["u--circle"]} />
                         </g>
@@ -151,7 +151,7 @@ export function SpinnerCircles() {
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                 >
-                    <g className={css["g-circles g-circles--v4"]}>
+                    <g className={`${css["g-circles"]} ${css["g-circles--v4"]}`}>
                         <g className={css["g--circle"]}>
                             <use xlinkHref="#s--circle" className={css["u--circle"]} />
                         </g>

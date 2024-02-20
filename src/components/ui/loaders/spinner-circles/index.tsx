ffa644e53769cde6bb142c384aa4ad093f8a76a2
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-20 14:07:51 -0800

    WIP. svg circle loaders. initital commit

---
+++ src/components/ui/loaders/spinner-circles/index.tsx
@@ -0,0 +1,195 @@
+import css from "./circles.module.css"; // https://codepen.io/yoksel/pen/KKqeyj 'Thinking about SVG-preloaders'
+
+export function SpinnerCircles() {
+    return (
+        <div className="p-4 text-green-950 flex items-center justify-center">
+
+            <div className="l-wrapper">
+                <svg
+                    viewBox="0 0 120 120"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                    <symbol id="s--circle">
+                        <circle r={10} cx={20} cy={20} />
+                    </symbol>
+                    <g className="g-circles g-circles--v1">
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                    </g>
+                </svg>
+                <svg
+                    viewBox="0 0 120 120"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                    <g className="g-circles g-circles--v2">
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                    </g>
+                </svg>
+                <svg
+                    viewBox="0 0 120 120"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                    <g className="g-circles g-circles--v3">
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                    </g>
+                </svg>
+                <svg
+                    viewBox="0 0 120 120"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                    <g className="g-circles g-circles--v4">
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                        <g className="g--circle">
+                            <use xlinkHref="#s--circle" className="u--circle" />
+                        </g>
+                    </g>
+                </svg>
+            </div>
+
+        </div>
+    );
+}

826827532e449e39ea4a5ebb3a360b1b2aba5a89
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-24 15:49:54 -0800

    WIP. tables.. switch to use table data w/ loop

--- src/components/2-main/2-demo/10-demo-tables/3-sticky-headers/sticky.tsx
+++ src/components/2-main/2-demo/10-demo-tables/3-sticky-headers/sticky.tsx
@@ -1,6 +1,8 @@
 import styles from "./sticky.module.css";
 import { D1, D10, D2, D3, D4, D5, D6, D7, D8, D9 } from "./tables-data";
 
+const cells = [D1, D2, D3, D4, D5, D6, D7, D8, D9, D10];
+
 function Table1() { // NOTE: there should be a space inside tr children
     return (
         <>
@@ -18,7 +20,15 @@ function Table1() { // NOTE: there should be a space inside tr children
                             </tr>
                         </thead>
                         <tbody>
-                            <tr><td>1</td><D1 /></tr>
+                            {
+                                cells.map((Cell, i) => (
+                                    <tr key={i}>
+                                        <td>{i + 1}</td>
+                                        <Cell />
+                                    </tr>
+                                ))
+                            }
+                            {/* <tr><td>1</td><D1 /></tr>
                             <tr><td>2</td><D2 /></tr>
                             <tr><td>3</td><D3 /></tr>
                             <tr><td>4</td><D4 /></tr>
@@ -27,7 +37,7 @@ function Table1() { // NOTE: there should be a space inside tr children
                             <tr><td>7</td><D7 /></tr>
                             <tr><td>8</td><D8 /></tr>
                             <tr><td>9</td><D9 /></tr>
-                            <tr><td>10</td><D10 /></tr>
+                            <tr><td>10</td><D10 /></tr> */}
                         </tbody>
                     </table>
                 </div>
@@ -41,7 +51,7 @@ function Table2() {
         <>
             <section className={`${styles["sticky-first-column"]}`}>
                 <h2>Sticky First Column</h2>
-                <div className={`${styles["container"]} w-60 overflow-auto`}>
+                <div className={`${styles["container"]} w-60 h-60 overflow-auto`}>
                     <table>
                         <thead>
                             <tr>
@@ -53,11 +63,19 @@ function Table2() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr><th scope="row">1</th><D1 /></tr>
+                            {
+                                cells.slice(0, 5).map((Cell, i) => (
+                                    <tr key={i}>
+                                        <th scope="row">{i + 1}</th>
+                                        <Cell />
+                                    </tr>
+                                ))
+                            }
+                            {/* <tr><th scope="row">1</th><D1 /></tr>
                             <tr><th scope="row">2</th><D2 /></tr>
                             <tr><th scope="row">3</th><D3 /></tr>
                             <tr><th scope="row">4</th><D4 /></tr>
-                            <tr><th scope="row">5</th><D5 /></tr>
+                            <tr><th scope="row">5</th><D5 /></tr> */}
                         </tbody>
                     </table>
                 </div>
@@ -83,7 +101,15 @@ function Table3() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr><th scope="row">1</th><D1 /></tr>
+                            {
+                                cells.map((Cell, i) => (
+                                    <tr key={i}>
+                                        <th scope="row">{i + 1}</th>
+                                        <Cell />
+                                    </tr>
+                                ))
+                            }
+                            {/* <tr><th scope="row">1</th><D1 /></tr>
                             <tr><th scope="row">2</th><D2 /></tr>
                             <tr><th scope="row">3</th><D3 /></tr>
                             <tr><th scope="row">4</th><D4 /></tr>
@@ -92,7 +118,7 @@ function Table3() {
                             <tr><th scope="row">7</th><D7 /></tr>
                             <tr><th scope="row">8</th><D8 /></tr>
                             <tr><th scope="row">9</th><D9 /></tr>
-                            <tr><th scope="row">10</th><D10 /></tr>
+                            <tr><th scope="row">10</th><D10 /></tr> */}
                         </tbody>
                     </table>
                 </div>
@@ -103,7 +129,7 @@ function Table3() {
 
 export function TablesWithStyckyHeaderModules() {
     return (
-        <div className="text-xs flex flex-col space-y-4">
+        <div className="text-xs flex flex-wrap gap-4">
             <Table1 />
             <Table2 />
             <Table3 />

a70898b186fc6ca7dfebc471f456fed20ef1ba35
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-25 16:29:08 -0800

    WIP. clock. connect proper classes

--- src/components/2-main/2-demo/70-pickers/50-flip-clock/flip-clock.tsx
+++ src/components/2-main/2-demo/70-pickers/50-flip-clock/flip-clock.tsx
@@ -1,4 +1,4 @@
-import styles from "./flip-clock.module.css";
+import css from "./flip-clock.module.css";
 
 export function FlipClock() {
     return (
@@ -9,46 +9,22 @@ export function FlipClock() {
                 <tracker v-for="tracker in trackers" :property="tracker" :time="time" v-ref:trackers />
             </div> */}
 
-            <div className="flip-clock" data-date="2017-02-11">
-                <span className="flip-clock__piece" style={{}}>
+            <div className={css["flip-clock"]} data-date="2017-02-11">
+                <span className={css["flip-clock__piece"]} style={{}}>
                     <TwoDigits front={0} back={0} />
-                    {/* <span className="flip-clock__card flip-card">
-                        <b className="flip-card__top">00</b>
-                        <b className="flip-card__bottom" data-value={00} />
-                        <b className="flip-card__back" data-value={00} />
-                        <b className="flip-card__back-bottom" data-value={00} />
-                    </span> */}
-                    <span className="flip-clock__slot">Days</span>
+                    <span className={css["flip-clock__slot"]}>Days</span>
                 </span>
-                <span className="flip-clock__piece flip" style={{}}>
+                <span className={`${css["flip-clock__piece"]} ${css["flip"]}`} style={{}}>
                     <TwoDigits front={7} back={7} />
-                    {/* <span className="flip-clock__card flip-card">
-                        <b className="flip-card__top">07</b>
-                        <b className="flip-card__bottom" data-value={07} />
-                        <b className="flip-card__back" data-value={00} />
-                        <b className="flip-card__back-bottom" data-value={00} />
-                    </span> */}
-                    <span className="flip-clock__slot">Hours</span>
+                    <span className={css["flip-clock__slot"]}>Hours</span>
                 </span>
-                <span className="flip-clock__piece flip" style={{}}>
+                <span className={`${css["flip-clock__piece"]} ${css["flip"]}`} style={{}}>
                     <TwoDigits front={46} back={0} />
-                    {/* <span className="flip-clock__card flip-card">
-                        <b className="flip-card__top">46</b>
-                        <b className="flip-card__bottom" data-value={46} />
-                        <b className="flip-card__back" data-value={00} />
-                        <b className="flip-card__back-bottom" data-value={00} />
-                    </span> */}
-                    <span className="flip-clock__slot">Minutes</span>
+                    <span className={css["flip-clock__slot"]}>Minutes</span>
                 </span>
-                <span className="flip-clock__piece flip" style={{}}>
+                <span className={`${css["flip-clock__piece"]} ${css["flip"]}`} style={{}}>
                     <TwoDigits front={20} back={21} />
-                    {/* <span className="flip-clock__card flip-card">
-                        <b className="flip-card__top">20</b>
-                        <b className="flip-card__bottom" data-value={20} />
-                        <b className="flip-card__back" data-value={21} />
-                        <b className="flip-card__back-bottom" data-value={21} />
-                    </span> */}
-                    <span className="flip-clock__slot">Seconds</span>
+                    <span className={css["flip-clock__slot"]}>Seconds</span>
                 </span>
             </div>
 
@@ -56,11 +32,11 @@ export function FlipClock() {
     );
 }
 function TwoDigits({ front, back }: { front: number, back: number; }) {
-    return <span className="flip-clock__card flip-card">
-        <b className="flip-card__top">{front}</b>
-        <b className="flip-card__bottom" data-value={front} />
-        <b className="flip-card__back" data-value={back} />
-        <b className="flip-card__back-bottom" data-value={back} />
+    return <span className={`${css["flip-clock__card"]} ${css["flip-card"]}`}>
+        <b className={css["flip-card__top"]}>{front}</b>
+        <b className={css["flip-card__bottom"]} data-value={front} />
+        <b className={css["flip-card__back"]} data-value={back} />
+        <b className={css["flip-card__back-bottom"]} data-value={back} />
     </span>;
 }
 

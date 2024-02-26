3e0765bd8107380c323121df51b127d89918b5d2
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-25 17:04:23 -0800

    WIP. clock. add update interval

--- src/components/2-main/2-demo/70-pickers/50-flip-clock/flip-clock.tsx
+++ src/components/2-main/2-demo/70-pickers/50-flip-clock/flip-clock.tsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react";
 import css from "./flip-clock.module.css";
+import { Button } from "@/components/ui/shadcn";
 
 export function FlipClock() {
+    const [count, setCount] = useState(0);
+    const [isActivated, setIsActivated] = useState(false);
+
+    const currentTime = Date.now();
+    const time = {
+        days: Math.floor(currentTime / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((currentTime / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((currentTime / 1000 / 60) % 60),
+        seconds: Math.floor((currentTime / 1000) % 60)
+    };
+
+    useEffect(() => {
+        if (!isActivated) {
+            return;
+        }
+
+        const interval = setInterval(() => {
+            setCount((count) => count + 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [isActivated]);
+
     return (
-        <div>
+        <div className="flex flex-col gap-4">
+            <Button variant="outline" size="sm" onClick={() => setIsActivated(!isActivated)}>{isActivated ? "Stop" : "Start"}</Button>
             {/* <div className="flip-clock" data-date="2017-02-11"
             // @click="update"
             >
@@ -11,19 +36,19 @@ export function FlipClock() {
 
             <div className={css["flip-clock"]} data-date="2017-02-11">
                 <span className={css["flip-clock__piece"]} style={{}}>
-                    <TwoDigits front={0} back={0} />
+                    <Tracker front={count} back={count + 1} />
                     <span className={css["flip-clock__slot"]}>Days</span>
                 </span>
                 <span className={`${css["flip-clock__piece"]} ${css["flip"]}`} style={{}}>
-                    <TwoDigits front={7} back={7} />
+                    <Tracker front={7} back={7} />
                     <span className={css["flip-clock__slot"]}>Hours</span>
                 </span>
                 <span className={`${css["flip-clock__piece"]} ${css["flip"]}`} style={{}}>
-                    <TwoDigits front={46} back={0} />
+                    <Tracker front={46} back={0} />
                     <span className={css["flip-clock__slot"]}>Minutes</span>
                 </span>
                 <span className={`${css["flip-clock__piece"]} ${css["flip"]}`} style={{}}>
-                    <TwoDigits front={20} back={21} />
+                    <Tracker front={20} back={21} />
                     <span className={css["flip-clock__slot"]}>Seconds</span>
                 </span>
             </div>
@@ -31,7 +56,8 @@ export function FlipClock() {
         </div>
     );
 }
-function TwoDigits({ front, back }: { front: number, back: number; }) {
+
+function Tracker({ front, back }: { front: number, back: number; }) {
     return <span className={`${css["flip-clock__card"]} ${css["flip-card"]}`}>
         <b className={css["flip-card__top"]}>{front}</b>
         <b className={css["flip-card__bottom"]} data-value={front} />
@@ -39,4 +65,3 @@ function TwoDigits({ front, back }: { front: number, back: number; }) {
         <b className={css["flip-card__back-bottom"]} data-value={back} />
     </span>;
 }
-

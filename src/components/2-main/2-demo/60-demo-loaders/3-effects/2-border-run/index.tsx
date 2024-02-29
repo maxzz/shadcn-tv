feb620eeebac74dd2e501e664d4e60f6251e1a0f
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-29 14:46:08 -0800

    WIP. loaders. minor BorderRun tweaks

--- src/components/2-main/2-demo/60-demo-loaders/3-effects/2-border-run/index.tsx
+++ src/components/2-main/2-demo/60-demo-loaders/3-effects/2-border-run/index.tsx
@@ -2,20 +2,19 @@ import { HTMLAttributes } from "react";
 import css from "./border-run.module.css";
 import { classNames } from "@/utils";
 
-export default function BorderRun({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+export default function BorderRun({ className, ...rest }: HTMLAttributes<HTMLElement>) {
     return (
-        // <div className={classNames(css["container"], className)} {...rest}>
-            <button className={css["btn"]}>
+        <button className={classNames(css["btn"], "relative w-[180px] h-[60px]", className)} {...rest}>
 
-                <svg viewBox="0 0 180 60" className={css["border"]}>
-                    <polyline points="179,1 179,59 1,59 1,1 179,1" className={css["bg-line"]} />
-                    <polyline points="179,1 179,59 1,59 1,1 179,1" className={css["hl-line"]} />
-                </svg>
+            <svg viewBox="0 0 180 60" className={css["svg-border"]}>
+                <polyline points="179,1 179,59 1,59 1,1 179,1" className={css["bg-line"]} />
+                <polyline points="179,1 179,59 1,59 1,1 179,1" className={css["hl-line"]} />
+            </svg>
 
-                <span>
-                    HOVER ME
-                </span>
-            </button>
-        // </div>
+            <span>
+                HOVER ME
+            </span>
+
+        </button>
     );
 }

d7b49eee696c37a9db90f575ef71e89158bf14b6
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-28 16:31:49 -0800

    WIP. loaders. code clean up

--- src/components/2-main/9-showcases/cases/60-loaders.tsx
+++ src/components/2-main/9-showcases/cases/60-loaders.tsx
@@ -1,18 +1,18 @@
 import { Tabs, TabsContent, TabsList, TabsListWrapClasses, TabsTrigger } from "@/components/ui/shadcn";
 import { BubblesDemo, LoaderDotsRing, LoadersTest, Spinner7, Spinner9, SpinnerCircles, SpinnerDiv, SpinnerEmoji, SpinnerRadial, SpinnerSpherees, SpinnerSvg } from "../../2-demo/60-demo-loaders";
 
-export function CircleLoaders() {
+function SpinnerTabs() {
     return (
-        <Tabs defaultValue="loaders4" orientation="vertical">
+        <Tabs defaultValue="loaders1" orientation="vertical">
             <TabsList className={TabsListWrapClasses}>
-                <TabsTrigger value="loaders4">Spinner</TabsTrigger>
-                <TabsTrigger value="loaders6">Circles</TabsTrigger>
-                <TabsTrigger value="loaders1">Dots ring</TabsTrigger>
-                <TabsTrigger value="loaders5">Spinner7</TabsTrigger>
-                <TabsTrigger value="loaders3">Radial</TabsTrigger>
+                <TabsTrigger value="loaders1">Simple</TabsTrigger>
+                <TabsTrigger value="loaders2">Circles</TabsTrigger>
+                <TabsTrigger value="loaders3">Dots ring</TabsTrigger>
+                <TabsTrigger value="loaders4">Commet</TabsTrigger>
+                <TabsTrigger value="loaders5">Radar</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="loaders4">
+            <TabsContent value="loaders1">
                 <div className="my-8 flex items-center justify-center gap-8">
                     <SpinnerSvg className="w-8 h-8 stroke-[5] stroke-sky-500" />
                     <SpinnerDiv className="w-8 h-8" />
@@ -21,124 +21,74 @@ export function CircleLoaders() {
                 </div>
             </TabsContent>
 
-            <TabsContent value="loaders6">
+            <TabsContent value="loaders2">
                 <div className="my-8 flex items-center justify-center">
                     <SpinnerCircles />
                 </div>
             </TabsContent>
 
-            <TabsContent value="loaders1">
+            <TabsContent value="loaders3">
                 <div className="my-8 flex items-center justify-center">
                     <LoaderDotsRing />
                 </div>
             </TabsContent>
 
-            <TabsContent value="loaders5">
+            <TabsContent value="loaders4">
                 <div className="my-8 flex items-center justify-center">
                     <Spinner7 />
                 </div>
             </TabsContent>
 
-            <TabsContent value="loaders3">
+            <TabsContent value="loaders5">
                 <SpinnerRadial />
             </TabsContent>
-
         </Tabs>
     );
 }
 
-export function StyledLoaders() {
+function StyledTabs() {
     return (
-        <Tabs defaultValue="loaders8">
+        <Tabs defaultValue="loaders1">
             <TabsList className={TabsListWrapClasses}>
-            <TabsTrigger value="loaders1">Loaders</TabsTrigger>
-                <TabsTrigger value="loaders8">Bubbles</TabsTrigger>
-                <TabsTrigger value="loaders7">Spheres</TabsTrigger>
+                <TabsTrigger value="loaders1">Blocks</TabsTrigger>
+                <TabsTrigger value="loaders2">Bubbles</TabsTrigger>
+                <TabsTrigger value="loaders3">3D spheres</TabsTrigger>
             </TabsList>
 
             <TabsContent value="loaders1">
                 <LoadersTest className="m-auto max-w-sm flex items-center justify-between" />
             </TabsContent>
 
-            <TabsContent value="loaders8">
+            <TabsContent value="loaders2">
                 <div className="my-8 flex items-center justify-center">
                     <BubblesDemo />
                 </div>
             </TabsContent>
 
-            <TabsContent value="loaders7">
+            <TabsContent value="loaders3">
                 <div className="my-8 flex items-center justify-center">
                     <SpinnerSpherees />
                 </div>
             </TabsContent>
-
         </Tabs>
     );
 }
 
 export function LoadersInTab() {
     return (
-        <Tabs defaultValue="loaders8">
+        <Tabs defaultValue="loaders1">
             <TabsList className={TabsListWrapClasses}>
-                {/* <TabsTrigger value="loaders1">Loaders</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders2">Circle loader</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders3">Radial</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders4">Spinner</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders5">Spinner7</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders6">Circles</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders7">Spheres</TabsTrigger> */}
-                {/* <TabsTrigger value="loaders8">Bubbles</TabsTrigger> */}
-                <TabsTrigger value="loaders10">Circle group</TabsTrigger>
-                <TabsTrigger value="loaders9">Styled group</TabsTrigger>
+                <TabsTrigger value="loaders1">Spinners</TabsTrigger>
+                <TabsTrigger value="loaders2">Styled</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="loaders9">
-                <StyledLoaders />
-            </TabsContent>
-
             <TabsContent value="loaders10">
-                <CircleLoaders />
+                <SpinnerTabs />
             </TabsContent>
 
-            {/* <TabsContent value="loaders1">
-                <LoadersTest className="m-auto max-w-sm flex items-center justify-between" />
-            </TabsContent> */}
-
-            {/* <TabsContent value="loaders2">
-                <LoaderDotsRing />
-            </TabsContent> */}
-
-            {/* <TabsContent value="loaders3">
-                <SpinnerRadial />
-            </TabsContent> */}
-
-            {/* <TabsContent value="loaders4">
-                <div className="my-8 flex items-center justify-center gap-8">
-                    <SpinnerSvg className="w-8 h-8 stroke-[5] stroke-sky-500" />
-                    <SpinnerDiv className="w-8 h-8" />
-                    <Spinner9 className="w-8 h-8 text-sky-500" />
-                    <SpinnerEmoji className="text-4xl text-sky-500" />
-                </div>
-            </TabsContent> */}
-
-            {/* <TabsContent value="loaders6">
-                <div className="my-8 flex items-center justify-center">
-                    <SpinnerCircles />
-                </div>
-            </TabsContent> */}
-
-            {/* <TabsContent value="loaders7">
-                <div className="my-8 flex items-center justify-center">
-                    <SpinnerSpherees />
-                </div>
-            </TabsContent> */}
-
-            {/* <TabsContent value="loaders8">
-                <div className="my-8 flex items-center justify-center">
-                    <BubblesDemo />
-                </div>
-            </TabsContent> */}
-
+            <TabsContent value="loaders9">
+                <StyledTabs />
+            </TabsContent>
         </Tabs>
     );
 }

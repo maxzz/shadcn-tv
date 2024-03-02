c4924ab813169fa61e919783ed5f988f1f6cf36e
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-02 14:19:52 -0800

    WIP. toasts. vertical menu

--- src/components/2-main/9-showcases/cases/50-toasts.tsx
+++ src/components/2-main/9-showcases/cases/50-toasts.tsx
@@ -4,18 +4,29 @@ import { DialogDemoWoTrigger } from "../../2-demo/52-demo-toasts/6-dialog/20-dia
 
 export function ToastsInTabs() {
     return (
-        <div className="flex flex-col  gap-4">
-            <div className="flex items-center gap-2">
-                <SonnerDemo />
-                <RadixToastDemo />
-                <DrawerDemo />
+        <div className="flex justify-between">
+            <div className="flex flex-col  gap-4">
+                <div className="flex items-center gap-2">
+                    <SonnerDemo />
+                    <RadixToastDemo />
+                    <DrawerDemo />
+                </div>
+                <div className="flex items-center gap-2">
+                    <MenubarDemo />
+                    <MenuDropdownDemo />
+                </div>
+                <div className="flex items-center gap-2">
+                    <DialogDemoWoTrigger />
+                </div>
             </div>
-            <div className="flex items-center gap-2">
-                <MenubarDemo />
-                <MenuDropdownDemo />
-            </div>
-            <div className="flex items-center gap-2">
-                <DialogDemoWoTrigger />
+
+            {/* https://codepen.io/Prakash286/pen/dyRXNKG 'Exploring UI Animation #1' */}
+            <div className="relative text-[.5rem]">
+                <ul className="absolute w-96 rotate-90 origin-top-left text-foreground bg-sky-800/30 overflow-hidden">
+                    <li className="float-left mx-2">1-menu</li>
+                    <li className="float-left mx-2">2-menu</li>
+                    <li className="float-left mx-2">3-menu</li>
+                </ul>
             </div>
         </div>
     );

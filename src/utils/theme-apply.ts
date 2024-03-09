3d146604161f8f64f5279b0e29123d0da3def16b
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-08 19:04:46 -0800

    WIP. theme. update apply logic

--- src/utils/theme-apply.ts
+++ src/utils/theme-apply.ts
@@ -4,13 +4,28 @@ export function themeApplyMode(themeMode: ThemeMode): void {
 
     const root = window.document.documentElement;
 
-    root.classList.remove("light", "dark");
+    const newMode =
+        themeMode === "system"
+            ? window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "dark"
+                : ""
+            : themeMode === "dark"
+                ? "dark"
+                : "";
 
-    if (themeMode === "system") {
-        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-        root.classList.add(systemTheme);
-        return;
-    }
+    root.classList[!!newMode ? "add" : "remove"]("dark");
 
-    root.classList.add(themeMode);
+    // root.classList.remove("light", "dark");
+
+    // if (themeMode === "system") {
+    //     const isDarkNow = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    //     const systemTheme = isDarkNow ? "dark" : "light";
+    //     root.classList.add(systemTheme);
+    //     return;
+    // }
+    // root.classList.add(themeMode);
 }
+
+// export function setAppDarkMode(setDark: boolean) {
+//     document.getElementsByTagName('body')[0].classList[setDark ? 'add' : 'remove']('dark');
+// }

737c2cc26ea7709dcb6f058ebb73e6e2c09dff91
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-11 18:46:15 -0700

    WIP. select. update classes

--- src/components/ui/shadcn/select.tsx
+++ src/components/ui/shadcn/select.tsx
@@ -8,13 +8,15 @@ const Select = Prim.Root;
 const SelectGroup = Prim.Group;
 const SelectValue = Prim.Value;
 
-const SelectTriggerClasses = "\
+const selectTriggerClasses = "\
 px-3 py-2 h-9 w-full text-sm \
-border-input \
+\
 bg-transparent \
+placeholder:text-muted-foreground \
+\
+border-input \
 \
 ring-offset-background \
-placeholder:text-muted-foreground \
 \
 focus:outline-none \
 focus:ring-1 \
@@ -24,10 +26,12 @@ disabled:cursor-not-allowed \
 disabled:opacity-50 \
 \
 border rounded-md shadow-sm \
+\
 flex items-center justify-between";
+
 const SelectTrigger = forwardRef<ElementRef<typeof Prim.Trigger>, ComponentPropsWithoutRef<typeof Prim.Trigger>>(
     ({ className, children, ...rest }, ref) => (
-        <Prim.Trigger ref={ref} className={cn(SelectTriggerClasses, className)} {...rest}>
+        <Prim.Trigger ref={ref} className={cn(selectTriggerClasses, className)} {...rest}>
             {children}
             <Prim.Icon asChild>
                 <CaretSortIcon className="h-4 w-4 opacity-50" />
@@ -37,43 +41,47 @@ const SelectTrigger = forwardRef<ElementRef<typeof Prim.Trigger>, ComponentProps
 );
 SelectTrigger.displayName = Prim.Trigger.displayName;
 
-const SelectContentClasses = "\
+const selectContentClasses = "\
 z-50 relative min-w-[8rem] \
-bg-popover \
-text-popover-foreground \
+\
+text-popover-foreground bg-popover \
 \
 data-[state=open]:animate-in \
-data-[state=closed]:animate-out \
-data-[state=closed]:fade-out-0 \
 data-[state=open]:fade-in-0 \
-data-[state=closed]:zoom-out-95 \
 data-[state=open]:zoom-in-95 \
 \
+data-[state=closed]:animate-out \
+data-[state=closed]:fade-out-0 \
+data-[state=closed]:zoom-out-95 \
+\
+data-[side=top]:slide-in-from-bottom-2 \
+data-[side=right]:slide-in-from-left-2 \
 data-[side=bottom]:slide-in-from-top-2 \
 data-[side=left]:slide-in-from-right-2 \
-data-[side=right]:slide-in-from-left-2 \
-data-[side=top]:slide-in-from-bottom-2 \
 overflow-hidden \
 border rounded-md shadow-md";
-const SelectContentPopperClasses = "\
-data-[side=bottom]:translate-y-1 \
-data-[side=left]:-translate-x-1 \
+
+const selectContentPopperClasses = "\
+data-[side=top]:-translate-y-1 \
 data-[side=right]:translate-x-1 \
-data-[side=top]:-translate-y-1";
-const SelectContentViewportPopperClasses = "\
+data-[side=bottom]:translate-y-1 \
+data-[side=left]:-translate-x-1";
+
+const selectContentViewportPopperClasses = "\
 w-full \
 h-[var(--radix-select-trigger-height)] \
 min-w-[var(--radix-select-trigger-width)]";
-const SelectContent = forwardRef<React.ElementRef<typeof Prim.Content>, React.ComponentPropsWithoutRef<typeof Prim.Content>>(
+
+const SelectContent = forwardRef<ElementRef<typeof Prim.Content>, ComponentPropsWithoutRef<typeof Prim.Content>>(
     ({ className, children, position = "popper", ...rest }, ref) => (
         <Prim.Portal>
             <Prim.Content
                 ref={ref}
-                className={cn(SelectContentClasses, position === "popper" && SelectContentPopperClasses, className)}
+                className={cn(selectContentClasses, position === "popper" && selectContentPopperClasses, className)}
                 position={position}
                 {...rest}
             >
-                <Prim.Viewport className={cn("p-1", position === "popper" && SelectContentViewportPopperClasses)}>
+                <Prim.Viewport className={cn("p-1", position === "popper" && selectContentViewportPopperClasses)}>
                     {children}
                 </Prim.Viewport>
             </Prim.Content>
@@ -93,19 +101,22 @@ const SelectLabel = forwardRef<ElementRef<typeof Prim.Label>, ComponentPropsWith
 );
 SelectLabel.displayName = Prim.Label.displayName;
 
-const SelectItemClasses = "\
+const selectItemClasses = "\
 relative pl-2 pr-8 py-1.5 w-full text-sm \
-focus:bg-accent \
+\
 focus:text-accent-foreground \
+focus:bg-accent \
 \
 data-[disabled]:opacity-50 \
 data-[disabled]:pointer-events-none \
 \
 rounded-sm outline-none select-none cursor-default \
+\
 flex items-center";
+
 const SelectItem = forwardRef<ElementRef<typeof Prim.Item>, ComponentPropsWithoutRef<typeof Prim.Item>>(
     ({ className, children, ...rest }, ref) => (
-        <Prim.Item ref={ref} className={cn(SelectItemClasses, className)} {...rest} >
+        <Prim.Item ref={ref} className={cn(selectItemClasses, className)} {...rest} >
             <span className="absolute right-2 h-3.5 w-3.5 flex items-center justify-center">
                 <Prim.ItemIndicator>
                     <CheckIcon className="h-4 w-4" />

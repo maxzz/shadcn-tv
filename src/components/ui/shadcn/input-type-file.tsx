d5b2621e0b0fd0eaba764d22581e2c649c2fdb9d
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-10 13:53:55 -0700

    WIP. add InputFileAsDlg component

---
+++ src/components/ui/shadcn/input-type-file.tsx
@@ -0,0 +1,24 @@
+import { forwardRef, InputHTMLAttributes } from "react";
+
+type InputFileAsDlgProps = InputHTMLAttributes<HTMLInputElement> & {
+    openAsFolder?: boolean;   // folder mode: open folder
+    multiple?: boolean;     // files mode only: open multiple files
+};
+
+export const InputFileAsDlg = forwardRef<HTMLInputElement, InputFileAsDlgProps>(({ openAsFolder, multiple = true, ...rest }, ref) => {
+
+    const options = {
+        ...(openAsFolder && { webkitdirectory: '' }),
+    };
+
+    return (
+        <input
+            ref={ref}
+            className="hidden"
+            type="file"
+            multiple={multiple}
+            {...options}
+            {...rest}
+        />
+    );
+});

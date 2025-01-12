e5bfccccf527a630956aa0d40de4073fc08e3d92
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-12 15:08:15 -0800

    WIP. StepsDemo4. add react components. 1

--- src/components/2-main/2-demo/40-demo-controls/44-navigation/2-timeline/4-timeline/1-step-indicator.tsx
+++ src/components/2-main/2-demo/40-demo-controls/44-navigation/2-timeline/4-timeline/1-step-indicator.tsx
@@ -1,3 +1,5 @@
+import { type ReactNode } from "react";
+import { classNames } from "@/utils";
 import "./step-indicator.css";
 
 export function StepIndicator4() {
@@ -6,37 +8,65 @@ export function StepIndicator4() {
 
             <form>
                 <div className="steps">
-                    <div className="steps__step" data-step="0">
+                    <Step step={0} label="About You" />
+                    <div className="steps__connector"></div>
+
+                    <Step step={1} label="About Book" />
+                    <div className="steps__connector"></div>
+
+                    <Step step={2} label="Review" />
+                    <div className="steps__connector"></div>
+
+                    <Step step={3} label="Signing" />
+                    <div className="steps__connector"></div>
+                    
+                    <Step step={4} label="Contract" />
+
+                    {/* <div className="steps__step" data-step="0">
                         <div className="steps__step-number">1</div>
                         <div className="steps__step-name">About You</div>
                     </div>
                     <div className="steps__connector"></div>
+
                     <div className="steps__step" data-step="1">
                         <div className="steps__step-number">2</div>
                         <div className="steps__step-name">About Book</div>
                     </div>
                     <div className="steps__connector"></div>
+
                     <div className="steps__step" data-step="2">
                         <div className="steps__step-number">3</div>
                         <div className="steps__step-name">Review</div>
                     </div>
                     <div className="steps__connector"></div>
+
                     <div className="steps__step" data-step="3">
                         <div className="steps__step-number">4</div>
                         <div className="steps__step-name">Signing</div>
                     </div>
                     <div className="steps__connector"></div>
+
                     <div className="steps__step" data-step="4">
                         <div className="steps__step-number">5</div>
                         <div className="steps__step-name">Contract</div>
-                    </div>
+                    </div> */}
                 </div>
+
                 <div className="btn-group">
                     <button className="btn" type="button" data-action="prev" disabled>Previous</button>
                     <button className="btn" type="button" data-action="next">Next</button>
                 </div>
             </form>
 
+        </div>
+    );
+}
+
+function Step({ step, label }: { step: number; label: ReactNode; }) {
+    return (
+        <div className="steps__step" data-step={step}>
+            <div className="steps__step-number">{step}</div>
+            <div className="steps__step-name">{label}</div>
         </div>
     );
 }
--- src/components/2-main/2-demo/40-demo-controls/44-navigation/2-timeline/4-timeline/step-indicator-plain.ts
+++ src/components/2-main/2-demo/40-demo-controls/44-navigation/2-timeline/4-timeline/step-indicator-plain.ts
@@ -5,83 +5,83 @@
 // });
 
 class StepIndicator {
-	/** Element used for this step indicator */
-	el: HTMLElement | null;
-	/** Number of steps */
-	steps = 5;
+    /** Element used for this step indicator */
+    el: HTMLElement | null;
+    /** Number of steps */
+    steps = 5;
 
-	private _step = 0;
-	get step(): number {
-		return this._step;
-	}
-	set step(value: number) {
-		this.displayStep(value);
-		this._step = value;
-		this.checkExtremes();
-	}
-	/**
-	 * @param el CSS selector of the step indicator element
-	 */
-	constructor(el: string) {
-		this.el = document.querySelector(el);
-		document.addEventListener("click",this.clickAction.bind(this));
-		this.displayStep(this.step);
-		this.checkExtremes();
-	}
-	/**
-	 * @param e Click event
-	 */
-	clickAction(e: Event): void {
-		const button = e.target as HTMLButtonElement;
-		const actionName = button?.getAttribute("data-action");
+    private _step = 0;
+    get step(): number {
+        return this._step;
+    }
+    set step(value: number) {
+        this.displayStep(value);
+        this._step = value;
+        this.checkExtremes();
+    }
+    /**
+     * @param el CSS selector of the step indicator element
+     */
+    constructor(el: string) {
+        this.el = document.querySelector(el);
+        document.addEventListener("click", this.clickAction.bind(this));
+        this.displayStep(this.step);
+        this.checkExtremes();
+    }
+    /**
+     * @param e Click event
+     */
+    clickAction(e: Event): void {
+        const button = e.target as HTMLButtonElement;
+        const actionName = button?.getAttribute("data-action");
 
-		if (actionName === "prev") {
-			this.prev();
-		} else if (actionName === "next") {
-			this.next();
-		}
-	}
-	/** Go to the previous step. */
-	prev(): void {
-		if (this.step > 0) {
-			--this.step;
-		}
-	}
-	/** Go to the next step. */
-	next(): void {
-		if (this.step < this.steps - 1) {
-			++this.step;
-		}
-	}
-	/** Disable the Previous or Next button if hitting the first or last step. */
-	checkExtremes(): void {
-		const prevBtnEl = document.querySelector(`[data-action="prev"]`) as HTMLButtonElement;
-		const nextBtnEl = document.querySelector(`[data-action="next"]`) as HTMLButtonElement;
+        if (actionName === "prev") {
+            this.prev();
+        } else if (actionName === "next") {
+            this.next();
+        }
+    }
+    /** Go to the previous step. */
+    prev(): void {
+        if (this.step > 0) {
+            --this.step;
+        }
+    }
+    /** Go to the next step. */
+    next(): void {
+        if (this.step < this.steps - 1) {
+            ++this.step;
+        }
+    }
+    /** Disable the Previous or Next button if hitting the first or last step. */
+    checkExtremes(): void {
+        const prevBtnEl = document.querySelector(`[data-action="prev"]`) as HTMLButtonElement;
+        const nextBtnEl = document.querySelector(`[data-action="next"]`) as HTMLButtonElement;
 
-		if (prevBtnEl) {
-			prevBtnEl.disabled = this.step <= 0;
-		}
-		if (nextBtnEl) {
-			nextBtnEl.disabled = this.step >= this.steps - 1;
-		}
-	}
-	/**
-	 * Update the indicator for a targeted step.
-	 * @param targetStep Index of the step
-	 */
-	displayStep(targetStep: number) {
-		const current = "steps__step--current";
-		const done = "steps__step--done";
+        if (prevBtnEl) {
+            prevBtnEl.disabled = this.step <= 0;
+        }
+        if (nextBtnEl) {
+            nextBtnEl.disabled = this.step >= this.steps - 1;
+        }
+    }
+    /**
+     * Update the indicator for a targeted step.
+     * @param targetStep Index of the step
+     */
+    displayStep(targetStep: number) {
+        const currentClass = "steps__step--current";
+        const doneClass = "steps__step--done";
 
-		for (let s = 0; s < this.steps; ++s) {
-			const stepEl = this.el?.querySelector(`[data-step="${s}"]`);
-			stepEl?.classList.remove(current,done);
+        for (let idx = 0; idx < this.steps; ++idx) {
+            const stepEl = this.el?.querySelector(`[data-step="${idx}"]`);
+            stepEl?.classList.remove(currentClass, doneClass);
 
-			if (s < targetStep) {
-				stepEl?.classList.add(done);
-			} else if (s === targetStep) {
-				stepEl?.classList.add(current);
-			}
-		}
-	}
+            if (idx < targetStep) {
+                stepEl?.classList.add(doneClass);
+            } else if (idx === targetStep) {
+                stepEl?.classList.add(currentClass);
+            }
+        }
+    }
 }
